cb93ea5e0e6a17ab8116d5f13352b2847afe4924
fiorcode <45425686+fiorcode@users.noreply.github.com>
2020-09-17 20:00:58 -0300

    Create drive_provider.dart

---
+++ lib/providers/drive_provider.dart
@@ -0,0 +1,113 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import "package:http/http.dart" as http;
+import 'package:http/io_client.dart';
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:googleapis/drive/v3.dart' as dAPI;
+import 'package:google_sign_in/google_sign_in.dart';
+
+class DriveProvider with ChangeNotifier {
+  GoogleSignIn _googleSignIn;
+  GoogleSignInAccount _currentUser;
+  dAPI.FileList _fileList;
+
+  GoogleSignInAccount get currentUser => _currentUser;
+
+  Future<void> handleSignIn() async {
+    try {
+      await _googleSignIn.signIn();
+      _currentUser = _googleSignIn.currentUser;
+      notifyListeners();
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  Future<void> handleSignOut() => _googleSignIn.disconnect();
+
+  Future trySignInSilently() async {
+    _googleSignIn = GoogleSignIn(scopes: [dAPI.DriveApi.DriveAppdataScope]);
+    _googleSignIn.onCurrentUserChanged
+        .listen((GoogleSignInAccount account) => _currentUser = account);
+    await _googleSignIn.signInSilently();
+    _currentUser = _googleSignIn.currentUser;
+    notifyListeners();
+  }
+
+  Future<bool> downloadDB() async {
+    var client = GoogleHttpClient(await _currentUser.authHeaders);
+    var drive = dAPI.DriveApi(client);
+    await drive.files.list(spaces: 'appDataFolder').then((value) {
+      _fileList = value;
+      _fileList.files.forEach((f) async {
+        if (f.name == 'kw.db') {
+          dAPI.Media file = await drive.files.get(
+            f.id,
+            downloadOptions: dAPI.DownloadOptions.FullMedia,
+          );
+          print(f.id);
+          final dbPath = await sql.getDatabasesPath();
+          final localFile = File('$dbPath/kw.db');
+          List<int> dataStore = [];
+          file.stream.listen((data) {
+            dataStore.insertAll(dataStore.length, data);
+          }, onDone: () {
+            localFile.writeAsBytesSync(dataStore, flush: true);
+            return true;
+          }, onError: (error) {
+            print("Some Error");
+            return false;
+          });
+        }
+      });
+    });
+    return false;
+  }
+
+  Future<void> uploadDB() async {
+    try {
+      dAPI.File fileToUpload = dAPI.File();
+      fileToUpload.name = 'kw.db';
+      fileToUpload.parents = ["appDataFolder"];
+
+      final dbPath = await sql.getDatabasesPath();
+      final localFile = File('$dbPath/kw.db');
+
+      final client = GoogleHttpClient(await _currentUser.authHeaders);
+      final drive = dAPI.DriveApi(client);
+
+      await drive.files.create(
+        fileToUpload,
+        uploadMedia: dAPI.Media(
+          localFile.openRead(),
+          localFile.lengthSync(),
+        ),
+      );
+    } catch (error) {}
+  }
+
+  Future<void> deleteDB() async {
+    var client = GoogleHttpClient(await _currentUser.authHeaders);
+    var drive = dAPI.DriveApi(client);
+    drive.files.list(spaces: 'appDataFolder').then((value) {
+      value.files.forEach((f) {
+        if (f.name == 'kw.db') drive.files.delete(f.id);
+        print('${f.id} DELETED');
+      });
+    });
+  }
+}
+
+class GoogleHttpClient extends IOClient {
+  Map<String, String> _headers;
+  GoogleHttpClient(this._headers) : super();
+  @override
+  Future<IOStreamedResponse> send(http.BaseRequest request) =>
+      super.send(request..headers.addAll(_headers));
+
+  @override
+  Future<http.Response> head(Object url, {Map<String, String> headers}) =>
+      super.head(url, headers: headers..addAll(_headers));
+}
