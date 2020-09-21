72df8d3c2f6ebf2dc0f4cb96f336564d7ea1b6fc
fiorcode <45425686+fiorcode@users.noreply.github.com>
2020-09-21 20:11:00 -0300

    Update backup_screen.dart

--- lib/screens/backup_screen.dart
+++ lib/screens/backup_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:keyway/providers/drive_provider.dart';
+import 'package:keyway/widgets/backup_status_card.dart';
 
 import 'package:provider/provider.dart';
 
@@ -42,142 +43,45 @@ class SignedInBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Center(
-        child: Container(
-          height: MediaQuery.of(context).size.height -
-              Scaffold.of(context).appBarMaxHeight,
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: ListView(
+        children: [
+          Image(
+            image: AssetImage('assets/drive_logo.png'),
+            height: 96,
+          ),
+          SizedBox(height: 32),
+          BackupStatusCard(),
+          SizedBox(height: 32),
+          Center(
+            child: RaisedButton(
+              onPressed: () => drive.handleSignOut(),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
+                  children: <Widget>[
                     Image(
-                      image: AssetImage('assets/drive_logo.png'),
-                      height: 96,
+                      image: AssetImage('assets/google_logo.png'),
+                      height: 32,
                     ),
-                    SizedBox(height: 16),
-                    Card(
-                      elevation: 6,
-                      shadowColor: Colors.green,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                        side: BorderSide(width: 1, color: Colors.green),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text(
+                        'Sign Out',
+                        style: TextStyle(color: Colors.red),
                       ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            Text(
-                              'Google Drive File Status',
-                              style: TextStyle(
-                                color: Colors.black.withOpacity(0.6),
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                            SizedBox(height: 8),
-                            Icon(
-                              Icons.cloud_done,
-                              color: Colors.green,
-                              size: 64,
-                            ),
-                            const Text(
-                              'File Found',
-                              style: TextStyle(
-                                color: Colors.green,
-                              ),
-                            ),
-                            SizedBox(height: 8),
-                            Wrap(
-                              crossAxisAlignment: WrapCrossAlignment.center,
-                              alignment: WrapAlignment.spaceEvenly,
-                              spacing: 2,
-                              children: [
-                                Icon(
-                                  Icons.access_time,
-                                  color: Colors.black54,
-                                ),
-                                const Text(
-                                  'Last time uploaded: 27/09/2020',
-                                  style: TextStyle(
-                                    color: Colors.black54,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            ButtonBar(
-                              alignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                FlatButton(
-                                  onPressed: null,
-                                  child: Text(
-                                    'UPLOAD',
-                                    style: TextStyle(color: Colors.blue),
-                                  ),
-                                ),
-                                FlatButton(
-                                  onPressed: null,
-                                  child: Text(
-                                    'DOWNLOAD',
-                                    style: TextStyle(color: Colors.green),
-                                  ),
-                                ),
-                                FlatButton(
-                                  onPressed: null,
-                                  child: Text(
-                                    'DELETE',
-                                    style: TextStyle(color: Colors.red),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
+                    )
                   ],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: RaisedButton(
-                  onPressed: () => drive.handleSignOut(),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 12),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Image(
-                          image: AssetImage('assets/google_logo.png'),
-                          height: 32,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8.0),
-                          child: Text(
-                            'Sign Out',
-                            style: TextStyle(color: Colors.red),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(32),
-                  ),
-                ),
-              ),
-            ],
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(32)),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
