eceb4c0ce95704d3b253fd1cc02eada5b21000d3
fiorcode <45425686+fiorcode@users.noreply.github.com>
2020-10-03 15:58:41 -0300

    Update item_provider.dart

--- lib/providers/item_provider.dart
+++ lib/providers/item_provider.dart
@@ -9,56 +9,36 @@ class ItemProvider with ChangeNotifier {
 
   List<Item> get items => [..._items];
 
-  Future<void> fetchAndSetItems() async {
+  Future fetchAndSetItems() async {
     _items.clear();
     await _fetchAndSetAlpha();
     _items.sort((a, b) => b.date.compareTo(a.date));
-  }
-
-  Future<void> _fetchAndSetAlpha() async {
-    final _alphaList = await DBHelper.getData('alpha');
-    _items.addAll(_alphaList
-        .map(
-          (item) => AlphaItem(
-            id: item['id'],
-            title: item['title'],
-            username: item['username'],
-            password: item['password'],
-            pin: item['pin'],
-            ip: item['ip'],
-            date: item['date'],
-          ),
-        )
-        .toList());
     notifyListeners();
   }
 
-  void addAlpha(AlphaItem item) {
-    DBHelper.insert('alpha', {
-      'title': item.title,
-      'username': item.username,
-      'password': item.password,
-      'pin': item.pin,
-      'ip': item.ip,
-      'date': DateTime.now().toUtc().toIso8601String(),
-    });
+  Future _fetchAndSetAlpha() async {
+    final _alphaList = await DBHelper.getData('alpha');
+    _items.addAll(_alphaList.map((item) => AlphaItem.fromMap(item)).toList());
+  }
+
+  addAlpha(AlphaItem item) {
+    DBHelper.insert('alpha', item.toMap());
     fetchAndSetItems();
   }
 
-  void updateAlpha(AlphaItem item) {
+  updateAlpha(AlphaItem item) {
     DBHelper.update('alpha', item.toMap());
     fetchAndSetItems();
   }
 
-  Future<void> deleteAlpha(int id) async {
+  Future deleteAlpha(int id) async {
     await DBHelper.delete('alpha', id); //THIS COULD FAIL
     _items.removeWhere((element) => element.id == id);
-    notifyListeners();
+    fetchAndSetItems();
   }
 
-  void removeItems() {
-    DBHelper.removeDB();
-    _items.clear();
-    notifyListeners();
+  removeItems() async {
+    await DBHelper.removeDB();
+    fetchAndSetItems();
   }
 }
