31f1e73894fb67a38973d0c60a21abe64315124e
fiorcode <45425686+fiorcode@users.noreply.github.com>
2020-10-06 18:14:24 -0300

    Update item_provider.dart

--- lib/providers/item_provider.dart
+++ lib/providers/item_provider.dart
@@ -9,19 +9,20 @@ class ItemProvider with ChangeNotifier {
 
   List<Item> get items => [..._items];
 
-  Future fetchAndSetItems() async {
+  fetchAndSetItems() async {
     _items.clear();
     await _fetchAndSetAlpha();
+    await _checkExpirations();
     _items.sort((a, b) => b.date.compareTo(a.date));
     notifyListeners();
   }
 
-  Future _fetchAndSetAlpha() async {
+  _fetchAndSetAlpha() async {
     final _alphaList = await DBHelper.getData('alpha');
     _items.addAll(_alphaList.map((item) => AlphaItem.fromMap(item)).toList());
   }
 
-  addAlpha(AlphaItem item) async {
+  insertAlpha(AlphaItem item) async {
     await DBHelper.insert('alpha', item.toMap());
     fetchAndSetItems();
   }
@@ -31,9 +32,12 @@ class ItemProvider with ChangeNotifier {
     fetchAndSetItems();
   }
 
-  Future deleteAlpha(int id) async {
-    await DBHelper.delete('alpha', id); //THIS COULD FAIL
-    _items.removeWhere((element) => element.id == id);
+  deleteAlpha(AlphaItem item) async {
+    if (item.repeated == 'n')
+      await DBHelper.delete('alpha', item.id);
+    else
+      await DBHelper.deleteRepeated(item.toMap());
+    _items.removeWhere((element) => element.id == item.id);
     fetchAndSetItems();
   }
 
@@ -45,9 +49,26 @@ class ItemProvider with ChangeNotifier {
       return false;
   }
 
-  Future checkRepeated() async {
-    List<AlphaItem> _alphaItems = _items.cast<AlphaItem>();
-    _alphaItems.sort((a, b) => b.password.compareTo(a.password));
+  _checkExpirations() async {
+    _items.cast<AlphaItem>().forEach((i) async {
+      i.expired = 'n';
+      i.dateTime = DateTime.parse(i.date);
+      if (i.dateTime.difference(DateTime.now()).inMinutes.abs() > 10) {
+        i.expired = 'y';
+        await DBHelper.update('alpha', i.toMap());
+      }
+    });
+  }
+
+  insertRepeated(AlphaItem item) async {
+    await DBHelper.insert('alpha', item.toMap());
+    await DBHelper.updateRepeated(item.toMap());
+    fetchAndSetItems();
+  }
+
+  deleteRepeated(AlphaItem item) async {
+    await DBHelper.deleteRepeated(item.toMap());
+    fetchAndSetItems();
   }
 
   removeItems() async {
