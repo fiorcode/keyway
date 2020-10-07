a68916cb11bd599cfc244dc24db7bd9f4a0055ce
fiorcode <45425686+fiorcode@users.noreply.github.com>
2020-10-07 15:40:30 -0300

    Create error_body.dart

---
+++ lib/widgets/error_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ErrorBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset("assets/ic_error.png"),
+    );
+  }
+}
