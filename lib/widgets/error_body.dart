fb5efa6c8806fdc775ae0d4337cd518c1b1db89a
fiorcode <45425686+fiorcode@users.noreply.github.com>
2020-10-09 13:16:29 -0300

    Update error_body.dart

--- lib/widgets/error_body.dart
+++ lib/widgets/error_body.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
 class ErrorBody extends StatelessWidget {
+  ErrorBody(this.message);
+
+  final String message;
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Image.asset("assets/ic_error.png"),
+    return Column(
+      children: [
+        Center(
+            child: Padding(
+          padding: const EdgeInsets.all(64.0),
+          child: Image.asset("assets/error.png"),
+        )),
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Text(
+              message,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.red),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
