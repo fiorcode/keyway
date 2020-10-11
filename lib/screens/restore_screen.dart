3e398fedc61b54769b3d8dbccda9e78a677d9846
fiorcode <45425686+fiorcode@users.noreply.github.com>
2020-10-11 13:29:57 -0300

    Update restore_screen.dart

--- lib/screens/restore_screen.dart
+++ lib/screens/restore_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:keyway/helpers/error_helper.dart';
 import 'package:provider/provider.dart';
 
+import 'package:keyway/helpers/error_helper.dart';
 import 'package:keyway/providers/drive_provider.dart';
 import 'package:keyway/widgets/restore_card.dart';
 
