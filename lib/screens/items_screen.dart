751c50cb6017e0a6398d095c5b0adad262494bbe
fiorcode <45425686+fiorcode@users.noreply.github.com>
2020-10-15 17:52:30 -0300

    Update items_screen.dart

--- lib/screens/items_screen.dart
+++ lib/screens/items_screen.dart
@@ -36,11 +36,13 @@ class _ItemsListScreenState extends State<ItemsListScreen> {
       backgroundColor: Theme.of(context).backgroundColor,
       appBar: AppBar(
         centerTitle: true,
-        leading: IconButton(
-          icon: Icon(Icons.settings),
-          onPressed: () =>
-              Navigator.of(context).pushNamed(DashboardScreen.routeName),
-        ),
+        leading: cripto.locked
+            ? null
+            : IconButton(
+                icon: Icon(Icons.settings),
+                onPressed: () =>
+                    Navigator.of(context).pushNamed(DashboardScreen.routeName),
+              ),
         title: cripto.locked
             ? IconButton(
                 icon: Icon(
@@ -52,14 +54,16 @@ class _ItemsListScreenState extends State<ItemsListScreen> {
             : IconButton(
                 icon: Icon(Icons.lock_open, color: Colors.green),
                 onPressed: null),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              Navigator.of(context).pushNamed(AlphaScreen.routeName);
-            },
-          )
-        ],
+        actions: cripto.locked
+            ? null
+            : [
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(AlphaScreen.routeName);
+                  },
+                )
+              ],
       ),
       body: FutureBuilder(
         future: elements.fetchAndSetItems(),
@@ -67,43 +71,44 @@ class _ItemsListScreenState extends State<ItemsListScreen> {
                 ConnectionState.waiting
             ? Center(child: CircularProgressIndicator())
             : Consumer<ItemProvider>(
-                child: Stack(
-                  children: [
-                    Center(
-                      child: Icon(
-                        Icons.blur_on,
-                        size: 128,
-                        color: Colors.white54,
-                      ),
-                    ),
-                    if (_unlocking && cripto.locked)
-                      Container(
-                        child: BackdropFilter(
-                          filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
-                          child: Container(
-                            color: Colors.black.withOpacity(0.1),
-                            child: Center(
-                              child: Padding(
-                                padding: const EdgeInsets.all(32),
-                                child: UnlockTextField(_lockSwitch),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                  ],
+                child: Center(
+                  child: Icon(
+                    Icons.blur_on,
+                    size: 128,
+                    color: Colors.white54,
+                  ),
                 ),
                 builder: (ctx, lib, ch) => lib.items.length <= 0
                     ? ch
-                    : ListView.builder(
-                        padding: EdgeInsets.all(8),
-                        itemCount: lib.items.length,
-                        itemBuilder: (ctx, i) {
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 1),
-                            child: AlphaCard(lib.items[i]),
-                          );
-                        },
+                    : Stack(
+                        children: [
+                          ListView.builder(
+                            padding: EdgeInsets.all(8),
+                            itemCount: lib.items.length,
+                            itemBuilder: (ctx, i) {
+                              return Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 1),
+                                child: AlphaCard(lib.items[i]),
+                              );
+                            },
+                          ),
+                          if (_unlocking && cripto.locked)
+                            Container(
+                              child: BackdropFilter(
+                                filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+                                child: Container(
+                                  color: Colors.black.withOpacity(0.1),
+                                  child: Center(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(32),
+                                      child: UnlockTextField(_lockSwitch),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                        ],
                       ),
               ),
       ),
