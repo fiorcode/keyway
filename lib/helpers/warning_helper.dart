1d232ec92c406e006caf98e181ed3de4772b609a
fiorcode <45425686+fiorcode@users.noreply.github.com>
2020-10-17 20:32:46 -0300

    Create warning_helper.dart

---
+++ lib/helpers/warning_helper.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class WarningHelper {
+  passRepeatedWarning(BuildContext context, Function f) async {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Password repeated', textAlign: TextAlign.center),
+          content: Text(
+            'This password is already in use, do you want to save it anyway?',
+            textAlign: TextAlign.center,
+          ),
+          actions: <Widget>[
+            FlatButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('CANCEL'),
+            ),
+            FlatButton(
+              onPressed: f,
+              child: Text('SAVE', style: TextStyle(color: Colors.red)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
