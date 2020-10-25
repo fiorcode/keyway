8cac877219ca5b7e50100c30f7fad6f786b6b02a
fiorcode <45425686+fiorcode@users.noreply.github.com>
2020-10-25 15:49:41 -0300

    Update backup_status_card.dart

--- lib/widgets/backup_status_card.dart
+++ lib/widgets/backup_status_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
 
 import 'package:keyway/providers/drive_provider.dart';
 
