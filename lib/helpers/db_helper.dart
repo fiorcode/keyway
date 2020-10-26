f4812d56876caf3a853f977f9dbd9790bc35f8c9
fiorcode <45425686+fiorcode@users.noreply.github.com>
2020-10-26 20:07:01 -0300

    Update db_helper.dart

--- lib/helpers/db_helper.dart
+++ lib/helpers/db_helper.dart
@@ -4,12 +4,19 @@ import 'package:sqflite/sqflite.dart' as sql;
 import 'package:sqflite/sqlite_api.dart';
 
 class DBHelper {
+  static const String userDataTable = "user_data";
+  static const String itemsTable = "items";
+  static const String oldItemsTable = "old_items";
+  static const String deletedItemsTable = "deleted_items";
+
   static Future<Database> database() async {
     final dbPath = await sql.getDatabasesPath();
     return sql.openDatabase(path.join(dbPath, 'kw.db'),
         onCreate: (db, version) async {
-      await db.execute(createAlphaTable);
+      await db.execute(createItemsTable);
       await db.execute(createUserDataTable);
+      await db.execute(createOldItemsTable);
+      await db.execute(createDeletedItemsTable);
     }, version: 1);
   }
 
@@ -38,21 +45,24 @@ class DBHelper {
     db.delete(table, where: 'id = ?', whereArgs: [id]);
   }
 
-  static Future<List<Map<String, dynamic>>> getByValue(
-      String table, String col, String val) async {
+  static Future<List<Map<String, dynamic>>> getItemById(int id) async {
     final db = await DBHelper.database();
-    return db.query(table, where: '$col = ?', whereArgs: [val]);
+    return db.query(itemsTable, where: 'id = ?', whereArgs: [id]);
   }
 
-  static Future<void> removeDB() async {
-    final dbPath = await sql.getDatabasesPath();
-    File('$dbPath/kw.db').delete();
+  static Future<List<Map<String, dynamic>>> getByValue(
+    String table,
+    String col,
+    String val,
+  ) async {
+    final db = await DBHelper.database();
+    return db.query(table, where: '$col = ?', whereArgs: [val]);
   }
 
   static Future checkRepeated(Map<String, Object> data) async {
     final db = await DBHelper.database();
     return await db.update(
-      'alpha',
+      itemsTable,
       {'repeated': 'y'},
       where: 'password = ?',
       whereArgs: [data['password']],
@@ -61,12 +71,12 @@ class DBHelper {
 
   static Future deleteRepeated(Map<String, Object> data) async {
     final db = await DBHelper.database();
-    await delete('alpha', data['id']);
+    await delete(itemsTable, data['id']);
     List<Map<String, dynamic>> _list =
-        await getByValue('alpha', 'password', data['password']);
+        await getByValue(itemsTable, 'password', data['password']);
     if (_list.length < 2) {
       return await db.update(
-        'alpha',
+        itemsTable,
         {'repeated': 'n'},
         where: 'password = ?',
         whereArgs: [data['password']],
@@ -74,13 +84,18 @@ class DBHelper {
     }
   }
 
-  static const createUserDataTable = '''CREATE TABLE user_data(
+  static Future<void> removeDB() async {
+    final dbPath = await sql.getDatabasesPath();
+    File('$dbPath/kw.db').delete();
+  }
+
+  static const createUserDataTable = '''CREATE TABLE $userDataTable(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     surname TEXT,
     enc_mk TEXT)''';
 
-  static const createAlphaTable = '''CREATE TABLE alpha(
+  static const createItemsTable = '''CREATE TABLE $itemsTable(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT,
     username TEXT,
@@ -92,4 +107,33 @@ class DBHelper {
     color INTEGER,
     repeated TEXT,
     expired TEXT)''';
+
+  static const createOldItemsTable = '''CREATE TABLE $oldItemsTable(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    username TEXT,
+    password TEXT,
+    pin TEXT,
+    ip TEXT,
+    date TEXT,
+    date_short TEXT,
+    color INTEGER,
+    repeated TEXT,
+    expired TEXT,
+    item_id INTEGER)''';
+
+  static const createDeletedItemsTable = '''CREATE TABLE $deletedItemsTable(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    username TEXT,
+    password TEXT,
+    pin TEXT,
+    ip TEXT,
+    date TEXT,
+    date_short TEXT,
+    date_deleted TEXT,
+    color INTEGER,
+    repeated TEXT,
+    expired TEXT,
+    item_id INTEGER)''';
 }
