3d3044cb205b2c03fbae8de64114c1191b2a6265
fiorcode <45425686+fiorcode@users.noreply.github.com>
2020-11-04 16:43:57 -0300

    Update dashboard_screen.dart

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -12,30 +12,34 @@ class DashboardScreen extends StatelessWidget {
       appBar: AppBar(),
       body: Container(
         color: Theme.of(context).backgroundColor,
-        child: ListView(
+        child: GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: 1.2,
           children: [
             DashboardCard(
-              icon: Icon(Icons.cloud_upload, color: Colors.white, size: 32),
+              icon: Icon(Icons.cloud_upload, color: Colors.white, size: 48),
               title: Text(
                 'Backup',
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 16,
-                ),
+                style: TextStyle(color: Colors.white, fontSize: 16),
               ),
               goTo: () =>
                   Navigator.of(context).pushNamed(BackupScreen.routeName),
             ),
             DashboardCard(
-              icon: Icon(Icons.storage, color: Colors.white, size: 32),
+              icon: Icon(Icons.storage, color: Colors.white, size: 48),
               title: Text(
                 'Data',
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 16,
-                ),
+                style: TextStyle(color: Colors.white, fontSize: 16),
               ),
               goTo: () => Navigator.of(context).pushNamed(DataScreen.routeName),
+            ),
+            DashboardCard(
+              icon: Icon(Icons.monetization_on, color: Colors.white, size: 48),
+              title: Text(
+                'Donate',
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+              goTo: () {},
             ),
           ],
         ),
