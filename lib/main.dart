64504252a75aaf1e5d2e87c4199f1cf4fc8864f9
fiorcode <45425686+fiorcode@users.noreply.github.com>
2020-11-05 18:15:15 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 import 'providers/drive_provider.dart';
@@ -18,6 +19,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MultiProvider(
       providers: [
         ChangeNotifierProvider.value(value: CriptoProvider()),
