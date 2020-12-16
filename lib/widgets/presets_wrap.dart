d6187725ed1c978520ec3960cfe9becd65ea4ebf
fiorcode <fiorcode@users.noreply.github.com>
2020-12-16 20:23:56 -0300

    Update presets_wrap.dart

--- lib/widgets/presets_wrap.dart
+++ lib/widgets/presets_wrap.dart
@@ -7,20 +7,24 @@ class PresetsWrap extends StatelessWidget {
     @required this.password,
     @required this.pin,
     @required this.ip,
+    @required this.longText,
     @required this.usernameSwitch,
     @required this.passwordSwitch,
     @required this.pinSwitch,
     @required this.ipSwitch,
+    @required this.longTextSwitch,
   }) : super(key: key);
 
   final bool username;
   final bool password;
   final bool pin;
   final bool ip;
+  final bool longText;
   final Function usernameSwitch;
   final Function passwordSwitch;
   final Function pinSwitch;
   final Function ipSwitch;
+  final Function longTextSwitch;
 
   @override
   Widget build(BuildContext context) {
@@ -74,6 +78,17 @@ class PresetsWrap extends StatelessWidget {
           elevation: ip ? 8 : 0,
           heroTag: null,
           onPressed: ipSwitch,
+        ),
+        FloatingActionButton(
+          backgroundColor: longText ? Colors.white : Colors.grey,
+          child: Icon(
+            Icons.notes_rounded,
+            size: longText ? 32 : 24,
+            color: longText ? Colors.grey : Colors.white,
+          ),
+          elevation: longText ? 8 : 0,
+          heroTag: null,
+          onPressed: longTextSwitch,
         ),
       ],
     );
