5ac0b52b22f1b8d1afda71b28669042df7d3c759
fiorcode <fiorcode@users.noreply.github.com>
2020-12-16 20:23:36 -0300

    Update item_provider.dart

--- lib/providers/item_provider.dart
+++ lib/providers/item_provider.dart
@@ -19,7 +19,7 @@ class ItemProvider with ChangeNotifier {
 
   Future<void> fetchItems() async {
     Iterable<Alpha> _iter;
-    await DBHelper.getData(DBHelper.itemsTable).then((data) {
+    await DBHelper.getData(DBHelper.alphaTable).then((data) {
       _items.clear();
       _iter = data.map((e) => Alpha.fromMap(e));
     });
@@ -45,7 +45,7 @@ class ItemProvider with ChangeNotifier {
 
   Future<void> fetchItemHistory(int itemId) async {
     Iterable<OldAlpha> _iterIH;
-    await DBHelper.getByValue(DBHelper.oldsTable, 'item_id', itemId)
+    await DBHelper.getByValue(DBHelper.oldAlphaTable, 'item_id', itemId)
         .then((data) {
       _itemHistory.clear();
       _iterIH = data.map((e) => OldAlpha.fromMap(e));
@@ -56,7 +56,7 @@ class ItemProvider with ChangeNotifier {
 
   Future<void> fetchDeletedItems() async {
     Iterable<DeletedAlpha> _iterDI;
-    await DBHelper.getData(DBHelper.deletedTable).then((data) {
+    await DBHelper.getData(DBHelper.deletedAlphaTable).then((data) {
       _deletedItems.clear();
       _iterDI = data.map((e) => DeletedAlpha.fromMap(e));
     });
@@ -64,60 +64,64 @@ class ItemProvider with ChangeNotifier {
     _deletedItems.sort((a, b) => b.date.compareTo(a.date));
   }
 
-  Future<void> insert(Alpha item) async =>
-      await DBHelper.insert(DBHelper.itemsTable, item.toMap());
+  Future<void> insert(Alpha a) async =>
+      await DBHelper.insert(DBHelper.alphaTable, a.toMap());
 
-  Future<void> update(Alpha item) async {
-    await DBHelper.getItemById(item.id).then(
-      (value) async {
-        Alpha _a = Alpha.fromMap(value.first);
-        if (_a.savePrevious(item)) {
+  Future<void> update(Alpha a) async {
+    await DBHelper.getById(DBHelper.alphaTable, a.id).then(
+      (items) async {
+        Alpha _a = Alpha.fromMap(items.first);
+        if (_a.savePrevious(a)) {
           OldAlpha old = OldAlpha.fromAlpha(_a);
-          await DBHelper.insert(DBHelper.oldsTable, old.toMap());
+          await DBHelper.insert(DBHelper.oldAlphaTable, old.toMap());
         }
       },
     );
-    await DBHelper.update(DBHelper.itemsTable, item.toMap());
+    await DBHelper.update(DBHelper.alphaTable, a.toMap());
   }
 
   Future<void> delete(Alpha item) async {
-    if (item.repeated == 'n')
-      await DBHelper.delete(DBHelper.itemsTable, item.id);
+    // REFRESH REPEATED STATUS
+    if (item.passStatus != 'REPEATED')
+      await DBHelper.delete(DBHelper.alphaTable, item.id);
     else
       await DBHelper.refreshRepetedPassword(item.password);
     DeletedAlpha delete = DeletedAlpha.fromAlpha(item);
-    await DBHelper.insert(DBHelper.deletedTable, delete.toMap());
+    await DBHelper.insert(DBHelper.deletedAlphaTable, delete.toMap());
     _items.removeWhere((element) => element.id == item.id);
   }
 
   Future<bool> verifyRepeatedPass(String s) async {
     if (s.isEmpty) return false;
-    if ((await DBHelper.getByValue(DBHelper.itemsTable, 'password', s))
+    if ((await DBHelper.getByValue(DBHelper.alphaTable, 'password', s))
         .isNotEmpty) return true;
-    if ((await DBHelper.getByValue(DBHelper.oldsTable, 'password', s))
+    if ((await DBHelper.getByValue(DBHelper.oldAlphaTable, 'password', s))
         .isNotEmpty) return true;
-    if ((await DBHelper.getByValue(DBHelper.deletedTable, 'password', s))
+    if ((await DBHelper.getByValue(DBHelper.deletedAlphaTable, 'password', s))
         .isNotEmpty) return true;
     return false;
   }
 
   Future<bool> verifyRepeatedPin(String p) async {
     if (p.isEmpty) return false;
-    if ((await DBHelper.getByValue(DBHelper.itemsTable, 'pin', p)).isNotEmpty)
+    if ((await DBHelper.getByValue(DBHelper.alphaTable, 'pin', p)).isNotEmpty)
       return true;
-    if ((await DBHelper.getByValue(DBHelper.oldsTable, 'pin', p)).isNotEmpty)
-      return true;
-    if ((await DBHelper.getByValue(DBHelper.deletedTable, 'pin', p)).isNotEmpty)
-      return true;
+    if ((await DBHelper.getByValue(DBHelper.oldAlphaTable, 'pin', p))
+        .isNotEmpty) return true;
+    if ((await DBHelper.getByValue(DBHelper.deletedAlphaTable, 'pin', p))
+        .isNotEmpty) return true;
     return false;
   }
 
-  Future<void> setRepeated(String column, String value) async =>
-      await DBHelper.setItemRepeated(column, value);
+  Future<void> setPassStatus(String pass, String status) async =>
+      await DBHelper.setPassStatus(DBHelper.alphaTable, pass, status);
+
+  Future<void> setPinStatus(String pin, String status) async =>
+      await DBHelper.setPinStatus(DBHelper.alphaTable, pin, status);
 
   Future<void> refreshRepetedPassword(String p) async {
     List<Map<String, dynamic>> _list =
-        await DBHelper.getByValue(DBHelper.itemsTable, 'password', p);
+        await DBHelper.getByValue(DBHelper.alphaTable, 'password', p);
     if (_list.length < 2) DBHelper.refreshRepetedPassword(p);
   }
 
@@ -132,13 +136,13 @@ class ItemProvider with ChangeNotifier {
     _items.cast<Alpha>().forEach((i) async {
       i.dateTime = DateTime.parse(i.date);
       int _diff = i.dateTime.difference(DateTime.now()).inDays.abs();
-      if (_diff > 180 && i.expired != 'y') {
-        i.expired = 'y';
-        await DBHelper.update(DBHelper.itemsTable, i.toMap());
+      if (_diff > 180 && i.expired != 'YES') {
+        i.expired = 'YES';
+        await DBHelper.update(DBHelper.alphaTable, i.toMap());
       }
-      if (_diff <= 180 && i.expired == 'y') {
-        i.expired = 'n';
-        await DBHelper.update(DBHelper.itemsTable, i.toMap());
+      if (_diff <= 180 && i.expired == 'YES') {
+        i.expired = '';
+        await DBHelper.update(DBHelper.alphaTable, i.toMap());
       }
     });
   }
@@ -189,19 +193,30 @@ class ItemProvider with ChangeNotifier {
     DateFormat dateFormat = DateFormat('dd/MM/yyyy H:mm');
     await _cripto.unlock('Qwe123!');
     for (int i = 0; i < _titles.length; i++) {
-      DateTime _date = DateTime(2020, _ran.nextInt(11) + 1);
-      insert(Alpha(
+      DateTime _date = DateTime(
+        2020,
+        _ran.nextInt(11) + 1,
+        _ran.nextInt(27) + 1,
+      );
+      insert(
+        Alpha(
           title: _titles[i],
           username: _cripto.doCrypt(_users[i]),
           password: _cripto.doCrypt(_passes[i]),
           pin: '',
           ip: '',
-          strong: _titles[i].contains('g') ? 'TRUE' : 'FALSE',
+          longText: '',
           date: _date.toIso8601String(),
           shortDate: dateFormat.format(_date),
           color: _ran.nextInt(4294967295),
-          repeated: 'n',
-          expired: _date.month < 7 ? 'y' : 'n'));
+          colorLetter: _ran.nextInt(4294967295),
+          passStatus: _titles[i].contains('g') ? 'REPEATED' : '',
+          pinStatus: '',
+          passLevel: _titles[i].contains('g') ? 'WEAK' : 'STRONG',
+          expired: _date.month < 7 ? 'YES' : 'NO',
+          expiredLapse: '${_ran.nextInt(365).toString()} DAYS',
+        ),
+      );
     }
   }
 
