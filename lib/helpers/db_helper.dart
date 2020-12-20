4bc086000aeba84347860d7da65d3019996a9161
fiorcode <fiorcode@users.noreply.github.com>
2020-12-20 19:19:14 -0300

    Update db_helper.dart

--- lib/helpers/db_helper.dart
+++ lib/helpers/db_helper.dart
@@ -101,13 +101,25 @@ class DBHelper {
         whereArgs: [pin],
       );
 
-  static Future<void> setPassRepeted(String p) async =>
-      (await DBHelper.database()).update(
-        alphaTable,
-        {'pass_status': 'REPEATED'},
-        where: 'password = ? pass_status = ?',
-        whereArgs: [p, ''],
-      );
+  // static Future<void> setPassRepeted(String p) async =>
+  //     (await DBHelper.database()).update(
+  //       alphaTable,
+  //       {'pass_status': 'REPEATED'},
+  //       where: 'password = ? pass_status = ?',
+  //       whereArgs: [p, ''],
+  //     );
+
+  static Future<int> setPassRepeted(String p) async {
+    return await (await DBHelper.database()).rawUpdate(
+      '''UPDATE 
+        $alphaTable
+        SET pass_status = ?
+        WHERE password = ? 
+        AND pass_status = ?''',
+      ['REPEATED', '$p', '""'],
+    );
+    //return ch;
+  }
 
   static Future<List<Map<String, dynamic>>> getPassRepeted(String p) async =>
       (await DBHelper.database()).query(
