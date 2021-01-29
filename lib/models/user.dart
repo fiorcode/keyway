cc5f9671fb2d0041fb3f1d48bbde43537701a0ec
fiorcode <fiorcode@users.noreply.github.com>
2021-01-29 17:45:50 -0300

    Update user.dart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,15 @@
 class User {
+  int id;
   String name, surname;
   String encMK, mkIV;
 
   User({this.name, this.surname, this.encMK, this.mkIV});
+
+  User.fromMap(Map<String, dynamic> map) {
+    id = map['id'];
+    name = map['name'];
+    surname = map['surname'];
+    encMK = map['encrypted_mk'];
+    mkIV = map['mk_iv'];
+  }
 }
