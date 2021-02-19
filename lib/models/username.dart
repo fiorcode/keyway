ada040c11f0160a846c343c17991becf6c225a79
fiorcode <fiorcode@users.noreply.github.com>
2021-02-19 20:26:52 -0300

    Create username.dart

---
+++ lib/models/username.dart
@@ -0,0 +1,11 @@
+class Username {
+  String username;
+  String usernameIV;
+  String usernameHash;
+
+  Username(this.username, this.usernameIV, this.usernameHash) {
+    this.username = '';
+    this.usernameIV = '';
+    this.usernameHash = '';
+  }
+}
