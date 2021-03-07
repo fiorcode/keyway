a7c0a4119548211c284bec7bb8c7506e91657238
fiorcode <fiorcode@users.noreply.github.com>
2021-03-07 18:58:56 -0300

    Create tags_screen.dart

---
+++ lib/screens/tags_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TagsScreen extends StatelessWidget {
+  static const routeName = '/tags';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
