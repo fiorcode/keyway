3a1fd645e2ff5817578e21a64eaf48a686d313c6
fiorcode <fiorcode@users.noreply.github.com>
2021-03-07 18:58:52 -0300

    Update tag_add_screen.dart

--- lib/screens/tag_add_screen.dart
+++ lib/screens/tag_add_screen.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
 
-import 'package:keyway/models/tag.dart';
+import '../widgets/TextFields/tag_text_field.dart';
 
 class TagAddScreen extends StatelessWidget {
   static const routeName = '/tag-add';
 
-  TagAddScreen(this.tagsInDB);
+  TagAddScreen({this.tagsChips});
 
-  final List<Tag> tagsInDB;
+  final List<Widget> tagsChips;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).backgroundColor,
+        iconTheme: IconThemeData(color: Theme.of(context).primaryColor),
+      ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [TextField(), Wrap()],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: TagTextField(),
+              ),
+              Wrap(children: tagsChips),
+            ],
+          ),
         ),
       ),
     );
