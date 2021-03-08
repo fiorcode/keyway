0b823f116c8b0560df53320433a274fb9c95fe7a
fiorcode <fiorcode@users.noreply.github.com>
2021-03-08 20:47:50 -0300

    Update tag_add_screen.dart

--- lib/screens/tag_add_screen.dart
+++ lib/screens/tag_add_screen.dart
@@ -1,13 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'package:keyway/models/tag.dart';
+import '../providers/item_provider.dart';
 import '../widgets/TextFields/tag_text_field.dart';
 
-class TagAddScreen extends StatelessWidget {
+class TagAddScreen extends StatefulWidget {
   static const routeName = '/tag-add';
 
-  TagAddScreen({this.tagsChips});
+  @override
+  _TagAddScreenState createState() => _TagAddScreenState();
+}
 
-  final List<Widget> tagsChips;
+class _TagAddScreenState extends State<TagAddScreen> {
+  ItemProvider _items;
+  Future<List<Tag>> _getTags;
+  List<Widget> _chips;
+
+  Future<List<Tag>> _tagsList() async => await _items.getTags();
+
+  List<Widget> _tags(List<Tag> tags) {
+    _chips = <Widget>[];
+    tags.forEach(
+      (tag) {
+        _chips.add(
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4.0),
+            child: Chip(
+              backgroundColor: Colors.grey,
+              label: Text(
+                tag.tagName,
+                style: TextStyle(color: Colors.white),
+              ),
+              elevation: 8.0,
+            ),
+          ),
+        );
+      },
+    );
+    return _chips;
+  }
+
+  @override
+  void initState() {
+    _items = Provider.of<ItemProvider>(context, listen: false);
+    _getTags = _tagsList();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +67,20 @@ class TagAddScreen extends StatelessWidget {
                 padding: const EdgeInsets.all(32.0),
                 child: TagTextField(),
               ),
-              Wrap(children: tagsChips),
+              FutureBuilder(
+                  future: _getTags,
+                  builder: (ctx, snap) {
+                    switch (snap.connectionState) {
+                      case ConnectionState.done:
+                        return Wrap(
+                          alignment: WrapAlignment.center,
+                          children: _tags(snap.data),
+                        );
+                        break;
+                      default:
+                        return Center(child: CircularProgressIndicator());
+                    }
+                  }),
             ],
           ),
         ),
