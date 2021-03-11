daf0fcc17add65ae593f2d7acfb88ae50fb38ac1
fiorcode <fiorcode@users.noreply.github.com>
2021-03-10 22:09:48 -0300

    Update color_alpha_picker.dart

--- lib/widgets/color_alpha_picker.dart
+++ lib/widgets/color_alpha_picker.dart
@@ -14,19 +14,17 @@ class _ColorAlphaPickerState extends State<ColorAlphaPicker> {
   double _value;
   Color _color;
 
-  _setColor(double val) {
-    setState(() => _color = _color.withAlpha(val.toInt()));
-  }
+  _setColor(double val) => _color = _color.withAlpha(val.toInt());
 
-  _setValue(Color col) {
-    setState(() => _value = _color.alpha.toDouble());
-  }
+  // _setValue(Color col) {
+  //   setState(() => _value = _color.alpha.toDouble());
+  // }
 
   @override
   void initState() {
     if (widget.color != null) {
       _color = widget.color;
-      _setValue(_color);
+      _value = _color.alpha.toDouble();
     } else {
       _color = Colors.grey;
       _value = 255;
