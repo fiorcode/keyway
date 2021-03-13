6bda8b4628dc7ccb97c7d6dfab53a66f9d40d5a8
fiorcode <fiorcode@users.noreply.github.com>
2021-03-13 17:20:54 -0300

    Update alpha_unlocked_card.dart

--- lib/widgets/Cards/alpha_unlocked_card.dart
+++ lib/widgets/Cards/alpha_unlocked_card.dart
@@ -133,7 +133,7 @@ class _AlphaUnlockedCardState extends State<AlphaUnlockedCard> {
   Color _setIconColor() {
     return (widget.alpha.passwordStatus == 'REPEATED' ||
             widget.alpha.pinStatus == 'REPEATED')
-        ? Colors.red
+        ? Colors.grey[200]
         : Colors.grey;
   }
 
