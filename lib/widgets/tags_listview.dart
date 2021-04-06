62c78a01f94f81123a58a6bc4eda8b05636424be
fiorcode <fiorcode@users.noreply.github.com>
2021-04-06 19:53:57 -0300

    Update tags_listview.dart

--- lib/widgets/tags_listview.dart
+++ lib/widgets/tags_listview.dart
@@ -92,79 +92,82 @@ class _TagsListViewState extends State<TagsListView> {
           width: 3,
         ),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 8.0),
-            child: Text(
-              'Tags',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey[600],
+      child: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                'Tags',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[600],
+                ),
               ),
             ),
-          ),
-          Container(
-            height: 64.0,
-            child: FutureBuilder(
-                future: _getTags,
-                builder: (ctx, snap) {
-                  switch (snap.connectionState) {
-                    case ConnectionState.done:
-                      return (snap.data as List<Tag>).length > 0
-                          ? Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Expanded(
-                                  child: ListView(
-                                    padding:
-                                        EdgeInsets.symmetric(horizontal: 4.0),
-                                    scrollDirection: Axis.horizontal,
-                                    children: _tags(snap.data, true),
-                                  ),
-                                ),
-                                IconButton(
-                                  icon: Icon(
-                                    Icons.add_circle_outline_rounded,
-                                    size: 32,
-                                    color: Theme.of(context).primaryColor,
-                                  ),
-                                  onPressed: () => Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => TagAddScreen(),
+            Container(
+              height: 64.0,
+              child: FutureBuilder(
+                  future: _getTags,
+                  builder: (ctx, snap) {
+                    switch (snap.connectionState) {
+                      case ConnectionState.done:
+                        return (snap.data as List<Tag>).length > 0
+                            ? Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Expanded(
+                                    child: ListView(
+                                      padding:
+                                          EdgeInsets.symmetric(horizontal: 4.0),
+                                      scrollDirection: Axis.horizontal,
+                                      children: _tags(snap.data, true),
                                     ),
-                                  ).then((tag) => _onReturn(tag)),
+                                  ),
+                                  IconButton(
+                                    icon: Icon(
+                                      Icons.add_circle_outline_rounded,
+                                      size: 32,
+                                      color: Theme.of(context).primaryColor,
+                                    ),
+                                    onPressed: () => Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => TagAddScreen(),
+                                      ),
+                                    ).then((tag) => _onReturn(tag)),
+                                  ),
+                                ],
+                              )
+                            : TextButton(
+                                onPressed: () => Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => TagAddScreen(),
+                                  ),
+                                ).then((tag) => _onReturn(tag)),
+                                child: Text(
+                                  'ADD NEW TAG',
+                                  style: TextStyle(
+                                    color: Colors.grey[800],
+                                    fontSize: 16,
+                                  ),
                                 ),
-                              ],
-                            )
-                          : TextButton(
-                              onPressed: () => Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => TagAddScreen(),
-                                ),
-                              ).then((tag) => _onReturn(tag)),
-                              child: Text(
-                                'ADD NEW TAG',
-                                style: TextStyle(
-                                  color: Colors.grey[800],
-                                  fontSize: 16,
-                                ),
-                              ),
-                            );
-                      break;
-                    default:
-                      return Center(child: CircularProgressIndicator());
-                  }
-                }),
-          ),
-        ],
+                              );
+                        break;
+                      default:
+                        return Center(child: CircularProgressIndicator());
+                    }
+                  }),
+            ),
+          ],
+        ),
       ),
     );
   }
