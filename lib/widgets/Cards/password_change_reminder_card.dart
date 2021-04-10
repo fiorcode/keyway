7c2e00b3e450cbe2695e38687422938503e71fcd
fiorcode <fiorcode@users.noreply.github.com>
2021-04-10 19:54:45 -0300

    Update password_change_reminder_card.dart

--- lib/widgets/Cards/password_change_reminder_card.dart
+++ lib/widgets/Cards/password_change_reminder_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:keyway/helpers/date_helper.dart';
 
 import 'package:keyway/models/alpha.dart';
 
@@ -195,6 +196,19 @@ class _PasswordChangeReminderCardState
                     ),
                 ],
               ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.calendar_today),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Created: ' +
+                        DateHelper.shortDate(widget.alpha.passwordDate),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
