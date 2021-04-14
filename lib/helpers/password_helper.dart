564c142fe9cda9a356f55108b1a1bea40d940753
fiorcode <fiorcode@users.noreply.github.com>
2021-04-14 19:54:33 -0300

    Update password_helper.dart

--- lib/helpers/password_helper.dart
+++ lib/helpers/password_helper.dart
@@ -1,3 +1,5 @@
+import 'package:zxcvbn/zxcvbn.dart';
+
 class PasswordHelper {
   static bool minLong(String s) => s.length > 5;
   static bool maxLong(String s) => s.length < 33;
@@ -13,4 +15,47 @@ class PasswordHelper {
       hasUpp(s) &&
       hasNum(s) &&
       hasSpec(s);
+
+  // static String level(String s) {
+  //   if (s.length < 11) return 'very weak';
+  //   int _value = 50;
+  //   if (hasLow(s)) _value = _value + 10;
+  //   if (hasUpp(s)) _value = _value + 10;
+  //   if (hasNum(s)) _value = _value + 10;
+  //   if (hasSpec(s)) _value = _value + 10;
+  //   if (s.length > 19) _value = _value + 10;
+  //   switch (_value) {
+  //     case 55:
+  //       return 'weak';
+  //       break;
+  //     case 85:
+  //       return 'strong';
+  //       break;
+  //     case 100:
+  //       return 'very strong';
+  //       break;
+  //     default:
+  //   }
+  // }
+
+  static String level(String s) {
+    if (s.isEmpty) return '';
+    final result = Zxcvbn().evaluate(s);
+    switch (result.score.toInt()) {
+      case 1:
+        return 'weak';
+        break;
+      case 2:
+        return 'regular';
+        break;
+      case 3:
+        return 'strong';
+        break;
+      case 4:
+        return 'very strong';
+        break;
+      default:
+        return 'very weak';
+    }
+  }
 }
