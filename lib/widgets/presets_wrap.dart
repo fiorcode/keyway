89f1cb80eaa9b759a6f5f6a71732acf1c7971e28
fiorcode <fiorcode@users.noreply.github.com>
2021-05-08 17:19:52 -0300

    Update presets_wrap.dart

--- lib/widgets/presets_wrap.dart
+++ lib/widgets/presets_wrap.dart
@@ -1,113 +1,133 @@
 import 'package:flutter/material.dart';
 
-class PresetsWrap extends StatelessWidget {
-  const PresetsWrap({
-    Key key,
-    @required this.username,
-    @required this.password,
-    @required this.pin,
-    @required this.device,
-    @required this.longText,
-    @required this.usernameSwitch,
-    @required this.passwordSwitch,
-    @required this.pinSwitch,
-    @required this.longTextSwitch,
-    @required this.deviceSwitch,
-  }) : super(key: key);
+import '../models/item.dart';
+import '../models/username.dart';
+import '../models/password.dart';
+import '../models/pin.dart';
+import '../models/longText.dart';
+import '../models/device.dart';
 
-  final bool username;
-  final bool password;
-  final bool pin;
-  final bool device;
-  final bool longText;
-  final Function usernameSwitch;
-  final Function passwordSwitch;
-  final Function pinSwitch;
-  final Function longTextSwitch;
-  final Function deviceSwitch;
+class PresetsWrap extends StatelessWidget {
+  const PresetsWrap({Key key, @required this.item, this.refreshScreen})
+      : super(key: key);
+
+  final Item item;
+  final Function refreshScreen;
+
+  void _usernameSwitch() {
+    item.username = item.username != null ? null : Username();
+    refreshScreen();
+  }
+
+  void _passwordSwitch() {
+    item.password = item.password != null ? null : Password();
+    refreshScreen();
+  }
+
+  void _pinSwitch() {
+    item.pin = item.pin != null ? null : Pin();
+    refreshScreen();
+  }
+
+  void _longTextSwitch() {
+    item.longText = item.longText != null ? null : LongText();
+    refreshScreen();
+  }
+
+  void _deviceSwitch() {
+    item.device = item.device != null ? null : Device();
+    refreshScreen();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 92,
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4),
-            child: FloatingActionButton(
-              backgroundColor: username ? Colors.white : Colors.grey,
-              child: Icon(
-                Icons.account_circle,
-                size: username ? 32 : 24,
-                color: username ? Colors.grey : Colors.white,
-              ),
-              elevation: username ? 8 : 0,
-              heroTag: null,
-              onPressed: usernameSwitch,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4),
-            child: FloatingActionButton(
-              backgroundColor: password ? Colors.white : Colors.grey,
-              child: Text(
-                '*',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: password ? 32 : 24,
-                  fontWeight: FontWeight.bold,
-                  color: password ? Colors.grey : Colors.white,
+      child: Center(
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: true,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: FloatingActionButton(
+                backgroundColor:
+                    item.username != null ? Colors.white : Colors.grey,
+                child: Icon(
+                  Icons.account_circle,
+                  size: item.username != null ? 32 : 24,
+                  color: item.username != null ? Colors.grey : Colors.white,
                 ),
+                elevation: item.username != null ? 8 : 0,
+                heroTag: null,
+                onPressed: _usernameSwitch,
               ),
-              elevation: password ? 8 : 0,
-              heroTag: null,
-              onPressed: passwordSwitch,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4),
-            child: FloatingActionButton(
-              backgroundColor: pin ? Colors.white : Colors.grey,
-              child: Icon(
-                Icons.dialpad,
-                size: pin ? 32 : 24,
-                color: pin ? Colors.grey : Colors.white,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: FloatingActionButton(
+                backgroundColor:
+                    item.password != null ? Colors.white : Colors.grey,
+                child: Text(
+                  '*',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: item.password != null ? 32 : 24,
+                    fontWeight: FontWeight.bold,
+                    color: item.password != null ? Colors.grey : Colors.white,
+                  ),
+                ),
+                elevation: item.password != null ? 8 : 0,
+                heroTag: null,
+                onPressed: _passwordSwitch,
               ),
-              elevation: pin ? 8 : 0,
-              heroTag: null,
-              onPressed: pinSwitch,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4),
-            child: FloatingActionButton(
-              backgroundColor: longText ? Colors.white : Colors.grey,
-              child: Icon(
-                Icons.notes_rounded,
-                size: longText ? 32 : 24,
-                color: longText ? Colors.grey : Colors.white,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: FloatingActionButton(
+                backgroundColor: item.pin != null ? Colors.white : Colors.grey,
+                child: Icon(
+                  Icons.dialpad,
+                  size: item.pin != null ? 32 : 24,
+                  color: item.pin != null ? Colors.grey : Colors.white,
+                ),
+                elevation: item.pin != null ? 8 : 0,
+                heroTag: null,
+                onPressed: _pinSwitch,
               ),
-              elevation: longText ? 8 : 0,
-              heroTag: null,
-              onPressed: longTextSwitch,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4),
-            child: FloatingActionButton(
-              backgroundColor: device ? Colors.white : Colors.grey,
-              child: Icon(
-                Icons.router,
-                size: device ? 32 : 24,
-                color: device ? Colors.grey : Colors.white,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: FloatingActionButton(
+                backgroundColor:
+                    item.longText != null ? Colors.white : Colors.grey,
+                child: Icon(
+                  Icons.notes_rounded,
+                  size: item.longText != null ? 32 : 24,
+                  color: item.longText != null ? Colors.grey : Colors.white,
+                ),
+                elevation: item.longText != null ? 8 : 0,
+                heroTag: null,
+                onPressed: _longTextSwitch,
               ),
-              elevation: device ? 8 : 0,
-              heroTag: null,
-              onPressed: deviceSwitch,
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: FloatingActionButton(
+                backgroundColor:
+                    item.device != null ? Colors.white : Colors.grey,
+                child: Icon(
+                  Icons.router,
+                  size: item.device != null ? 32 : 24,
+                  color: item.device != null ? Colors.grey : Colors.white,
+                ),
+                elevation: item.device != null ? 8 : 0,
+                heroTag: null,
+                onPressed: _deviceSwitch,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
