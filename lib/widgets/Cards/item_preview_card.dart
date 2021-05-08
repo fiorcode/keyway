2a2bd2cd26d9732e640ed5654880b24699c973e5
fiorcode <fiorcode@users.noreply.github.com>
2021-05-08 17:19:48 -0300

    Update item_preview_card.dart

--- lib/widgets/Cards/item_preview_card.dart
+++ lib/widgets/Cards/item_preview_card.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-import 'package:keyway/models/item.dart';
-import '../Pickers/color_alpha_picker.dart';
-import '../Pickers/letter_blue_color_picker.dart';
-import '../Pickers/letter_color_alpha_picker.dart';
-import '../Pickers/letter_green_color_picker.dart';
-import '../Pickers/letter_red_color_picker.dart';
+import '../../models/item.dart';
 import '../Pickers/red_color_picker.dart';
 import '../Pickers/green_color_picker.dart';
 import '../Pickers/blue_color_picker.dart';
+import '../Pickers/color_alpha_picker.dart';
+import '../Pickers/letter_red_color_picker.dart';
+import '../Pickers/letter_green_color_picker.dart';
+import '../Pickers/letter_blue_color_picker.dart';
+import '../Pickers/letter_color_alpha_picker.dart';
 
 class ItemPreviewCard extends StatefulWidget {
   ItemPreviewCard(this.item);
@@ -22,6 +22,7 @@ class ItemPreviewCard extends StatefulWidget {
 class _ItemPreviewCardState extends State<ItemPreviewCard> {
   Color _fillColor = Colors.grey;
   Color _fontColor = Colors.white;
+  bool _setting = false;
   bool _editingBackground = true;
 
   _setFillColor(int color) {
@@ -48,147 +49,132 @@ class _ItemPreviewCardState extends State<ItemPreviewCard> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Theme.of(context).backgroundColor,
-      elevation: 8,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8.0),
-        side: BorderSide(
-          color: Theme.of(context).primaryColor,
-          width: 3,
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            'Preview',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey[600],
+            ),
+          ),
         ),
-      ),
-      child: Container(
-        padding: EdgeInsets.all(12),
-        width: double.infinity,
-        child: Column(
-          children: [
-            Text(
-              'Preview',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey[600],
+        Card(
+          clipBehavior: Clip.antiAlias,
+          elevation: 8,
+          shape: StadiumBorder(),
+          child: ListTile(
+            dense: true,
+            contentPadding: EdgeInsets.all(4),
+            leading: CircleAvatar(
+              radius: 24,
+              backgroundColor: _fillColor,
+              child: Text(
+                widget.item.title.isEmpty
+                    ? 'T'
+                    : widget.item.title.substring(0, 1).toUpperCase(),
+                style: TextStyle(
+                  color: _fontColor,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-            Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton.icon(
-                      icon: Icon(
-                        Icons.format_color_fill,
+            title: Text(
+              widget.item.title.isEmpty ? 'Title' : widget.item.title,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w300,
+                color: Colors.black,
+              ),
+            ),
+            onTap: null,
+            trailing: Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: SizedBox(
+                height: 48,
+                width: 48,
+                child: FloatingActionButton(
+                  backgroundColor: Colors.grey[200],
+                  child: Icon(Icons.tune, size: 24, color: Colors.grey),
+                  heroTag: null,
+                  onPressed: () => setState(() => _setting = !_setting),
+                ),
+              ),
+            ),
+          ),
+        ),
+        if (_setting)
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton.icon(
+                    icon: Icon(
+                      Icons.format_color_fill,
+                      color: _editingBackground ? Colors.grey : Colors.white,
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      elevation: 4.0,
+                      primary: _editingBackground ? Colors.white : Colors.grey,
+                    ),
+                    label: Text(
+                      'Fill',
+                      style: TextStyle(
                         color: _editingBackground ? Colors.grey : Colors.white,
                       ),
-                      style: ElevatedButton.styleFrom(
-                        elevation: 4.0,
-                        primary:
-                            _editingBackground ? Colors.white : Colors.grey,
-                      ),
-                      label: Text(
-                        'Fill',
-                        style: TextStyle(
-                          color:
-                              _editingBackground ? Colors.grey : Colors.white,
-                        ),
-                      ),
-                      onPressed: _editingBackground ? () {} : _editingSwitch,
                     ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton.icon(
-                      icon: Icon(
-                        Icons.format_color_text,
-                        color: !_editingBackground ? Colors.grey : Colors.white,
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        elevation: !_editingBackground ? 4.0 : 0,
-                        primary:
-                            !_editingBackground ? Colors.white : Colors.grey,
-                      ),
-                      label: Text(
-                        'Font',
-                        style: TextStyle(
-                          color:
-                              !_editingBackground ? Colors.grey : Colors.white,
-                        ),
-                      ),
-                      onPressed: !_editingBackground ? () {} : _editingSwitch,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Card(
-              clipBehavior: Clip.antiAlias,
-              elevation: 8,
-              shape: StadiumBorder(),
-              child: ListTile(
-                dense: true,
-                contentPadding: EdgeInsets.all(4),
-                leading: CircleAvatar(
-                  radius: 24,
-                  backgroundColor: _fillColor,
-                  child: Text(
-                    widget.item.title.isEmpty
-                        ? 'T'
-                        : widget.item.title.substring(0, 1).toUpperCase(),
-                    style: TextStyle(
-                      color: _fontColor,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                title: Text(
-                  widget.item.title.isEmpty ? 'Title' : widget.item.title,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 1,
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.w300,
-                    color: Colors.black,
-                  ),
-                ),
-                onTap: null,
-                trailing: Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: SizedBox(
-                    height: 48,
-                    width: 48,
-                    child: FloatingActionButton(
-                      backgroundColor: Colors.grey[200],
-                      child: Icon(
-                        Icons.remove_red_eye_outlined,
-                        size: 24,
-                        color: Colors.grey,
-                      ),
-                      heroTag: null,
-                      onPressed: null,
-                    ),
+                    onPressed: _editingBackground ? () {} : _editingSwitch,
                   ),
                 ),
               ),
-            ),
-            _editingBackground
-                ? RedColorPicker(_fillColor, _setFillColor)
-                : LetterRedColorPicker(_fontColor, _setFontColor),
-            _editingBackground
-                ? GreenColorPicker(_fillColor, _setFillColor)
-                : LetterGreenColorPicker(_fontColor, _setFontColor),
-            _editingBackground
-                ? BlueColorPicker(_fillColor, _setFillColor)
-                : LetterBlueColorPicker(_fontColor, _setFontColor),
-            _editingBackground
-                ? ColorAlphaPicker(_fillColor, _setFillColor)
-                : LetterColorAlphaPicker(_fontColor, _setFontColor),
-          ],
-        ),
-      ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton.icon(
+                    icon: Icon(
+                      Icons.format_color_text,
+                      color: !_editingBackground ? Colors.grey : Colors.white,
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      elevation: !_editingBackground ? 4.0 : 0,
+                      primary: !_editingBackground ? Colors.white : Colors.grey,
+                    ),
+                    label: Text(
+                      'Font',
+                      style: TextStyle(
+                        color: !_editingBackground ? Colors.grey : Colors.white,
+                      ),
+                    ),
+                    onPressed: !_editingBackground ? () {} : _editingSwitch,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        if (_setting)
+          _editingBackground
+              ? RedColorPicker(_fillColor, _setFillColor)
+              : LetterRedColorPicker(_fontColor, _setFontColor),
+        if (_setting)
+          _editingBackground
+              ? GreenColorPicker(_fillColor, _setFillColor)
+              : LetterGreenColorPicker(_fontColor, _setFontColor),
+        if (_setting)
+          _editingBackground
+              ? BlueColorPicker(_fillColor, _setFillColor)
+              : LetterBlueColorPicker(_fontColor, _setFontColor),
+        if (_setting)
+          _editingBackground
+              ? ColorAlphaPicker(_fillColor, _setFillColor)
+              : LetterColorAlphaPicker(_fontColor, _setFontColor),
+      ],
     );
   }
 }
