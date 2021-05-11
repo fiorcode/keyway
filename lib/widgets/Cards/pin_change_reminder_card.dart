d5394a05a8c8f0161ac09030710c209dabd6994b
fiorcode <fiorcode@users.noreply.github.com>
2021-05-11 17:12:39 -0300

    Update pin_change_reminder_card.dart

--- lib/widgets/Cards/pin_change_reminder_card.dart
+++ lib/widgets/Cards/pin_change_reminder_card.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:keyway/helpers/date_helper.dart';
 
-import 'package:keyway/models/pin.dart';
+import '../../models/pin.dart';
 
 class PinChangeReminderCard extends StatefulWidget {
   const PinChangeReminderCard({Key key, this.pin}) : super(key: key);
@@ -13,14 +12,11 @@ class PinChangeReminderCard extends StatefulWidget {
 }
 
 class _PinChangeReminderCardState extends State<PinChangeReminderCard> {
-  bool _changeReminder = true;
   bool _custom = false;
 
   @override
   void initState() {
-    if (widget.pin.pinLapse != 32 &&
-        widget.pin.pinLapse != 96 &&
-        widget.pin.pinLapse != 192) {
+    if (widget.pin.pinLapse != 96 && widget.pin.pinLapse != 320) {
       _custom = true;
     }
     super.initState();
@@ -28,187 +24,131 @@ class _PinChangeReminderCardState extends State<PinChangeReminderCard> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Theme.of(context).backgroundColor,
-      elevation: 8,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8.0),
-        side: BorderSide(
-          color: Theme.of(context).primaryColor,
-          width: 3,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          'PIN change reminder',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey[600],
+          ),
         ),
-      ),
-      child: Container(
-        padding: const EdgeInsets.all(12),
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
+        Wrap(
+          alignment: WrapAlignment.center,
+          crossAxisAlignment: WrapCrossAlignment.center,
+          spacing: 12.0,
           children: [
-            Switch(
-              activeColor: Colors.green,
-              value: _changeReminder,
-              onChanged: (value) => setState(() {
-                _changeReminder = value;
-                if (value) {
-                  _custom = false;
-                  widget.pin.pinLapse = 192;
-                } else {
-                  widget.pin.pinLapse = -1;
-                }
-              }),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'PIN change reminder',
+            ChoiceChip(
+              backgroundColor: Colors.grey,
+              selected: widget.pin.pinLapse == -1,
+              selectedColor: Colors.grey[200],
+              onSelected: (selected) => selected
+                  ? setState(() {
+                      widget.pin.pinLapse = -1;
+                      _custom = false;
+                    })
+                  : null,
+              label: Text(
+                'Never',
                 style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey[600],
+                  color: widget.pin.pinLapse == -1 ? Colors.grey : Colors.white,
+                  fontWeight:
+                      widget.pin.pinLapse == -1 ? FontWeight.bold : null,
                 ),
               ),
+              elevation: widget.pin.pinLapse == -1 ? 8.0 : 0.0,
             ),
-            if (_changeReminder)
-              Wrap(
-                alignment: WrapAlignment.center,
-                crossAxisAlignment: WrapCrossAlignment.center,
-                spacing: 12.0,
+            ChoiceChip(
+              backgroundColor: Colors.grey,
+              selected: widget.pin.pinLapse == 96,
+              selectedColor: Colors.grey[200],
+              onSelected: (selected) => selected
+                  ? setState(() {
+                      widget.pin.pinLapse = 96;
+                      _custom = false;
+                    })
+                  : null,
+              label: Text(
+                '96 Days',
+                style: TextStyle(
+                  color: widget.pin.pinLapse == 96 ? Colors.grey : Colors.white,
+                  fontWeight:
+                      widget.pin.pinLapse == 96 ? FontWeight.bold : null,
+                ),
+              ),
+              elevation: widget.pin.pinLapse == 96 ? 8.0 : 0.0,
+            ),
+            ChoiceChip(
+              backgroundColor: Colors.grey,
+              selected: widget.pin.pinLapse == 320,
+              selectedColor: Colors.grey[200],
+              onSelected: (selected) => selected
+                  ? setState(() {
+                      widget.pin.pinLapse = 320;
+                      _custom = false;
+                    })
+                  : null,
+              label: Text(
+                '320 Days',
+                style: TextStyle(
+                  color:
+                      widget.pin.pinLapse == 320 ? Colors.grey : Colors.white,
+                  fontWeight:
+                      widget.pin.pinLapse == 320 ? FontWeight.bold : null,
+                ),
+              ),
+              elevation: widget.pin.pinLapse == 320 ? 8.0 : 0.0,
+            ),
+            ChoiceChip(
+              backgroundColor: Colors.grey,
+              selected: _custom,
+              selectedColor: Colors.grey[200],
+              onSelected: (selected) => setState(() => _custom = selected),
+              label: Text(
+                'Custom',
+                style: TextStyle(
+                  color: _custom ? Colors.grey : Colors.white,
+                  fontWeight: _custom ? FontWeight.bold : null,
+                ),
+              ),
+              elevation: _custom ? 8.0 : 0.0,
+            ),
+            if (_custom)
+              Column(
                 children: [
-                  if (!_custom)
-                    ChoiceChip(
-                      backgroundColor: Colors.grey,
-                      selected: widget.pin.pinLapse == 32,
-                      selectedColor: Colors.white,
-                      onSelected: (selected) => selected
-                          ? setState(() {
-                              widget.pin.pinLapse = 32;
-                              _custom = false;
-                            })
-                          : null,
-                      label: Text(
-                        '32 Days',
-                        style: TextStyle(
-                          color: widget.pin.pinLapse == 32
-                              ? Colors.grey
-                              : Colors.white,
-                          fontWeight: widget.pin.pinLapse == 32
-                              ? FontWeight.bold
-                              : null,
-                        ),
-                      ),
-                      elevation: widget.pin.pinLapse == 32 ? 8.0 : 0.0,
+                  SizedBox(height: 8),
+                  Text(
+                    widget.pin.pinLapse.toString() + ' Days',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
                     ),
-                  if (!_custom)
-                    ChoiceChip(
-                      backgroundColor: Colors.grey,
-                      selected: widget.pin.pinLapse == 96,
-                      selectedColor: Colors.white,
-                      onSelected: (selected) => selected
-                          ? setState(() {
-                              widget.pin.pinLapse = 96;
-                              _custom = false;
-                            })
-                          : null,
-                      label: Text(
-                        '96 Days',
-                        style: TextStyle(
-                          color: widget.pin.pinLapse == 96
-                              ? Colors.grey
-                              : Colors.white,
-                          fontWeight: widget.pin.pinLapse == 96
-                              ? FontWeight.bold
-                              : null,
-                        ),
-                      ),
-                      elevation: widget.pin.pinLapse == 96 ? 8.0 : 0.0,
-                    ),
-                  if (!_custom)
-                    ChoiceChip(
-                      backgroundColor: Colors.grey,
-                      selected: widget.pin.pinLapse == 192,
-                      selectedColor: Colors.white,
-                      onSelected: (selected) => selected
-                          ? setState(() {
-                              widget.pin.pinLapse = 192;
-                              _custom = false;
-                            })
-                          : null,
-                      label: Text(
-                        '192 Days',
-                        style: TextStyle(
-                          color: widget.pin.pinLapse == 192
-                              ? Colors.grey
-                              : Colors.white,
-                          fontWeight: widget.pin.pinLapse == 192
-                              ? FontWeight.bold
-                              : null,
-                        ),
-                      ),
-                      elevation: widget.pin.pinLapse == 192 ? 8.0 : 0.0,
-                    ),
-                  ChoiceChip(
-                    backgroundColor: Colors.grey,
-                    selected: _custom,
-                    selectedColor: Colors.white,
-                    onSelected: (selected) =>
-                        setState(() => _custom = selected),
-                    label: Text(
-                      'Custom',
-                      style: TextStyle(
-                        color: _custom ? Colors.grey : Colors.white,
-                        fontWeight: _custom ? FontWeight.bold : null,
-                      ),
-                    ),
-                    elevation: _custom ? 8.0 : 0.0,
                   ),
-                  if (_custom)
-                    Column(
-                      children: [
-                        SizedBox(height: 8),
-                        Text(
-                          widget.pin.pinLapse.toString() + ' Days',
-                          style: TextStyle(
-                            fontSize: 32,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        SliderTheme(
-                          data: SliderTheme.of(context).copyWith(
-                            valueIndicatorTextStyle: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                          child: Slider(
-                            min: 0,
-                            max: 364,
-                            value: widget.pin.pinLapse.toDouble(),
-                            onChanged: (value) => setState(
-                              () => widget.pin.pinLapse = value.round(),
-                            ),
-                          ),
-                        ),
-                      ],
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      valueIndicatorTextStyle: TextStyle(
+                        color: Colors.white,
+                      ),
                     ),
+                    child: Slider(
+                      min: 0,
+                      max: 364,
+                      value: widget.pin.pinLapse.toDouble(),
+                      onChanged: (value) => setState(
+                        () => widget.pin.pinLapse = value.round(),
+                      ),
+                    ),
+                  ),
                 ],
               ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(Icons.calendar_today),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Created: ' + DateHelper.shortDate(widget.pin.dateTime),
-                  ),
-                ),
-              ],
-            )
           ],
         ),
-      ),
+      ],
     );
   }
 }
