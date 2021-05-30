ee46ebe95c4dd16252055ccdcf2015d2b47213c9
fiorcode <fiorcode@users.noreply.github.com>
2021-05-29 22:50:00 -0300

    Update password_helper.dart

--- lib/helpers/password_helper.dart
+++ lib/helpers/password_helper.dart
@@ -1,21 +1,42 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:flutter/services.dart';
+import 'package:keyway/models/word.dart';
 import 'package:zxcvbn/zxcvbn.dart';
+
 import '../models/password.dart';
 
 class PasswordHelper {
+  static List<String> _conectors = [
+    '!',
+    '#',
+    '%',
+    '^',
+    '&',
+    '*',
+    '.',
+    '?',
+    '<',
+    '>'
+  ];
   static bool minLong(String s) => s.length > 5;
   static bool maxLong(String s) => s.length < 33;
   static bool hasLow(String s) => s.contains(RegExp(r'[a-z]'));
   static bool hasUpp(String s) => s.contains(RegExp(r'[A-Z]'));
   static bool hasNum(String s) => s.contains(RegExp(r'[0-9]'));
-  static bool hasSpec(String s) =>
-      s.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
-  static bool isStrong(String s) =>
-      minLong(s) &&
-      maxLong(s) &&
-      hasLow(s) &&
-      hasUpp(s) &&
-      hasNum(s) &&
-      hasSpec(s);
+  static bool hasSpec(String s) {
+    return s.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
+  }
+
+  static bool isStrong(String s) {
+    return minLong(s) &&
+        maxLong(s) &&
+        hasLow(s) &&
+        hasUpp(s) &&
+        hasNum(s) &&
+        hasSpec(s);
+  }
 
   static ZxcvbnResult strength(String s, {Password password}) {
     if (s.isEmpty) return null;
@@ -33,6 +54,20 @@ class PasswordHelper {
     if (s.isEmpty) return <String>[];
     _sugs = Zxcvbn().evaluate(s).feedback.suggestions;
     return _sugs;
+  }
+
+  static Future<String> dicePassword() async {
+    List<Word> _wordList = <Word>[];
+    final _list =
+        jsonDecode(await rootBundle.loadString('assets/word_list_en.json'));
+    _list.forEach((w) {
+      _wordList.add(Word.fromJson(w));
+    });
+    return _wordList
+            .elementAt(Random.secure().nextInt(_wordList.length))
+            .value +
+        _conectors.elementAt(Random.secure().nextInt(_conectors.length)) +
+        _wordList.elementAt(Random.secure().nextInt(_wordList.length)).value;
   }
 }
 
