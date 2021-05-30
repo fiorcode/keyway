37ade96da8f572cfe0876aa952e6f5f509fd9395
fiorcode <fiorcode@users.noreply.github.com>
2021-05-29 22:50:03 -0300

    Create word.dart

---
+++ lib/models/word.dart
@@ -0,0 +1,10 @@
+class Word {
+  final String value;
+  final String freq;
+
+  Word({this.value, this.freq});
+
+  factory Word.fromJson(Map<String, dynamic> json) {
+    return Word(value: json['word'], freq: json['freq']);
+  }
+}
