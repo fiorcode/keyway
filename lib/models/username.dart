dbfe63a18aa0c26db44dc4332fd2d205ee433bd1
fiorcode <fiorcode@users.noreply.github.com>
2021-06-21 18:08:47 -0300

    Update username.dart

--- lib/models/username.dart
+++ lib/models/username.dart
@@ -2,23 +2,27 @@ class Username {
   int usernameId;
   String usernameEnc;
   String usernameIv;
+  String usernameHash;
 
   Username({
     this.usernameId,
     this.usernameEnc = '',
     this.usernameIv = '',
+    this.usernameHash = '',
   });
 
   Username.fromMap(Map<String, dynamic> map) {
     usernameId = map['username_id'];
     usernameEnc = map['username_enc'];
     usernameIv = map['username_iv'];
+    usernameHash = map['username_hash'];
   }
 
   Map<String, dynamic> toMap() {
     Map<String, dynamic> map = <String, dynamic>{
       'username_enc': usernameEnc,
       'username_iv': usernameIv,
+      'username_hash': usernameHash,
     };
     if (usernameId != null) map['username_id'] = usernameId;
     return map;
@@ -28,5 +32,6 @@ class Username {
         usernameId: this.usernameId,
         usernameEnc: this.usernameEnc,
         usernameIv: this.usernameIv,
+        usernameHash: this.usernameHash,
       );
 }
