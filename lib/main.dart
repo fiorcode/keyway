5afea93c8da0ace1fb5cc4819f5de4f5c7abd84f
fiorcode <fiorcode@users.noreply.github.com>
2021-06-21 18:08:21 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:keyway/screens/cpe23uri_screen.dart';
+import 'package:keyway/screens/devices_screen.dart';
 import 'package:provider/provider.dart';
 
 import 'providers/drive_provider.dart';
 import 'providers/cripto_provider.dart';
 import 'providers/item_provider.dart';
+import 'screens/addresses_screen.dart';
+import 'screens/cpe23uri_cve_screen.dart';
+import 'screens/cve_screen.dart';
 import 'screens/item_deleted_view_screen.dart';
 import 'screens/item_edit_screen.dart';
 import 'screens/item_view_screen.dart';
 import 'screens/items_with_old_passwords_screen.dart';
 import 'screens/item_history_screen.dart';
+import 'screens/pins_screen.dart';
 import 'screens/restore_screen.dart';
 import 'screens/splash_screen.dart';
 import 'screens/set_password_screen.dart';
@@ -21,9 +27,11 @@ import 'screens/data_screen.dart';
 import 'screens/items_deleted_screen.dart';
 import 'screens/tag_add_screen.dart';
 import 'screens/tags_screen.dart';
-import 'package:keyway/screens/all_items_screen.dart';
-import 'package:keyway/screens/item_password_screen.dart';
-import 'package:keyway/screens/passwords_screen.dart';
+import 'screens/all_items_screen.dart';
+import 'screens/item_password_screen.dart';
+import 'screens/passwords_screen.dart';
+import 'screens/usernames_screen.dart';
+import 'package:keyway/screens/notes_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -69,6 +77,14 @@ class MyApp extends StatelessWidget {
           AllItemsListScreen.routeName: (ctx) => AllItemsListScreen(),
           ItemPasswordListScreen.routeName: (ctx) => ItemPasswordListScreen(),
           PasswordsListScreen.routeName: (ctx) => PasswordsListScreen(),
+          UsernamesListScreen.routeName: (ctx) => UsernamesListScreen(),
+          PinsListScreen.routeName: (ctx) => PinsListScreen(),
+          NotesListScreen.routeName: (ctx) => NotesListScreen(),
+          AddressesListScreen.routeName: (ctx) => AddressesListScreen(),
+          DevicesListScreen.routeName: (ctx) => DevicesListScreen(),
+          Cpe23uriListScreen.routeName: (ctx) => Cpe23uriListScreen(),
+          Cpe23uriCveListScreen.routeName: (ctx) => Cpe23uriCveListScreen(),
+          CveListScreen.routeName: (ctx) => CveListScreen(),
         },
       ),
     );
