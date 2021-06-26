36d81d77bd5d74406c2681454fe94ce20170aab5
fiorcode <fiorcode@users.noreply.github.com>
2021-06-26 17:57:09 -0300

    Create cpe_ref.dart

---
+++ lib/models/cpe_ref.dart
@@ -0,0 +1,9 @@
+class CpeRef {
+  final String ref;
+  final String type;
+
+  CpeRef(this.ref, this.type);
+
+  factory CpeRef.fromJson(Map<String, dynamic> parsedJson) =>
+      CpeRef(parsedJson['ref'], parsedJson['type']);
+}
