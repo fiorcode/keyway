ed48d2088cb91e611b900ceed2b1ea6b10ba2758
fiorcode <fiorcode@users.noreply.github.com>
2021-06-28 20:17:53 -0300

    Update password.dart

--- lib/models/password.dart
+++ lib/models/password.dart
@@ -2,31 +2,31 @@ class Password {
   int passwordId;
   String passwordEnc;
   String passwordIv;
-  String strength;
-  String hash;
+  String passwordStrength;
+  String passwordHash;
 
   Password({
     this.passwordId,
     this.passwordEnc = '',
     this.passwordIv = '',
-    this.strength = '',
-    this.hash = '',
+    this.passwordStrength = '',
+    this.passwordHash = '',
   });
 
   Password.fromMap(Map<String, dynamic> map) {
     passwordId = map['password_id'];
     passwordEnc = map['password_enc'];
     passwordIv = map['password_iv'];
-    strength = map['strength'];
-    hash = map['hash'];
+    passwordStrength = map['password_strength'];
+    passwordHash = map['password_hash'];
   }
 
   Map<String, dynamic> toMap() {
     Map<String, dynamic> map = <String, dynamic>{
       'password_enc': passwordEnc,
       'password_iv': passwordIv,
-      'strength': strength,
-      'hash': hash,
+      'password_strength': passwordStrength,
+      'password_hash': passwordHash,
     };
     if (passwordId != null) map['password_id'] = passwordId;
     return map;
@@ -36,7 +36,7 @@ class Password {
         passwordId: this.passwordId,
         passwordEnc: this.passwordEnc,
         passwordIv: this.passwordIv,
-        strength: this.strength,
-        hash: this.hash,
+        passwordStrength: this.passwordStrength,
+        passwordHash: this.passwordHash,
       );
 }
