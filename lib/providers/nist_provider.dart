88b4fdb4c29f2121b4b739983901e8cb5e7e06a1
fiorcode <fiorcode@users.noreply.github.com>
2021-06-29 20:59:41 -0300

    Update nist_provider.dart

--- lib/providers/nist_provider.dart
+++ lib/providers/nist_provider.dart
@@ -4,22 +4,20 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 import '../models/cpe_body.dart';
-import '../models/cpe.dart';
 
 class NistProvider with ChangeNotifier {
   static const _baseUrl = 'https://services.nvd.nist.gov/rest/json';
 
-  Future<List<Cpe>> getCpesByCpeMatch({
+  Future<CpeBody> getCpesByCpeMatch({
     String type = '*',
     String trademark = '*',
     String model = '*',
   }) async {
     final urlString =
-        '$_baseUrl/cpes/1.0?cpeMatchString=cpe:2.3:$type:$trademark:$model&resultsPerPage=50';
+        '$_baseUrl/cpes/1.0?cpeMatchString=cpe:2.3:$type:$trademark:$model&resultsPerPage=100';
     final response = await http.get(Uri.parse(urlString));
     if (response.statusCode == 200) {
-      CpeBody _body = CpeBody.fromJson(jsonDecode(response.body));
-      return _body.result.cpes;
+      return CpeBody.fromJson(jsonDecode(response.body));
     } else {
       throw Exception('Failed. Status code: ${response.statusCode}');
     }
