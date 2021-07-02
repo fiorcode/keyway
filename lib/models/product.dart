35475518500e4d9c29a2451cb2971896827c1eaa
fiorcode <fiorcode@users.noreply.github.com>
2021-07-02 17:53:03 -0300

    Update product.dart

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -8,7 +8,6 @@ class Product {
   String productStatus;
   int trackVulnerabilities;
   String lastTracking;
-  int fkCpe23uriId;
 
   Product({
     this.productId,
@@ -20,7 +19,6 @@ class Product {
     this.productStatus = '',
     this.trackVulnerabilities = 1,
     this.lastTracking = '',
-    this.fkCpe23uriId,
   });
 
   bool get tracked => this.trackVulnerabilities == 1 ? true : false;
@@ -30,29 +28,27 @@ class Product {
       : this.trackVulnerabilities = 1;
 
   Product.fromMap(Map<String, dynamic> map) {
-    this.productId = map['prodcut_id'];
-    this.productType = map['prodcut_type'];
-    this.productTrademark = map['prodcut_trademark'];
-    this.productModel = map['prodcut_model'];
-    this.productVersion = map['prodcut_version'];
-    this.productUpdate = map['prodcut_update'];
-    this.productStatus = map['prodcut_status'];
+    this.productId = map['product_id'];
+    this.productType = map['product_type'];
+    this.productTrademark = map['product_trademark'];
+    this.productModel = map['product_model'];
+    this.productVersion = map['product_version'];
+    this.productUpdate = map['product_update'];
+    this.productStatus = map['product_status'];
     this.trackVulnerabilities = map['track_vulnerabilities'];
     this.lastTracking = map['last_tracking'];
-    this.fkCpe23uriId = map['fk_cpe23uri_id'];
   }
 
   Map<String, dynamic> toMap() {
     Map<String, dynamic> map = <String, dynamic>{
-      'prodcut_type': this.productType,
-      'prodcut_trademark': this.productTrademark,
-      'prodcut_model': this.productModel,
-      'prodcut_version': this.productVersion,
-      'prodcut_update': this.productUpdate,
-      'prodcut_status': this.productStatus,
+      'product_type': this.productType,
+      'product_trademark': this.productTrademark,
+      'product_model': this.productModel,
+      'product_version': this.productVersion,
+      'product_update': this.productUpdate,
+      'product_status': this.productStatus,
       'track_vulnerabilities': this.trackVulnerabilities,
       'last_tracking': this.lastTracking,
-      'fk_cpe23uri_id': this.fkCpe23uriId,
     };
     if (this.productId != null) map['product_id'] = this.productId;
     return map;
@@ -65,6 +61,7 @@ class Product {
         productModel: this.productModel,
         productVersion: this.productVersion,
         productUpdate: this.productUpdate,
-        fkCpe23uriId: this.fkCpe23uriId,
+        trackVulnerabilities: this.trackVulnerabilities,
+        lastTracking: this.lastTracking,
       );
 }
