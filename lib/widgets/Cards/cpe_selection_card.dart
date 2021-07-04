4eacf2946988597cbdaba503708be516b0c409c4
fiorcode <fiorcode@users.noreply.github.com>
2021-07-03 21:28:16 -0300

    Update cpe_selection_card.dart

--- lib/widgets/Cards/cpe_selection_card.dart
+++ lib/widgets/Cards/cpe_selection_card.dart
@@ -14,48 +14,97 @@ class CpeSelectionCard extends StatefulWidget {
 class _CpeSelectionCardState extends State<CpeSelectionCard> {
   bool _selected = false;
 
+  void _onTap() => setState(() => _selected = !_selected);
+
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Text(
-                      widget.cpe.titles[0].title,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
+      elevation: _selected ? 8.0 : 0.0,
+      child: InkWell(
+        onTap: _onTap,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            height: 120,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          widget.cpe.titles[0].title,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
                       ),
                     ),
+                    if (_selected) Icon(Icons.check_circle, color: Colors.green)
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Type'),
+                    SizedBox(width: 8.0),
+                    Text(
+                      widget.cpe.type,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Trademark'),
+                    SizedBox(width: 8.0),
+                    Text(
+                      widget.cpe.trademark,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Model'),
+                    SizedBox(width: 8.0),
+                    Flexible(
+                      child: Text(
+                        widget.cpe.model,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+                if (widget.cpe.hasVulns)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.bug_report, color: Colors.red),
+                      Text(
+                        'Vulnerabilities reported',
+                        style: TextStyle(color: Colors.red),
+                      ),
+                    ],
                   ),
-                  Text(
-                    widget.cpe.cpe23Uri,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child:
-                        Text('Last modified: ${widget.cpe.lastModifiedDate}'),
-                  ),
-                ],
-              ),
+              ],
             ),
-            Checkbox(
-              value: _selected,
-              onChanged: (selected) => setState(() => _selected = selected),
-              checkColor: Colors.green,
-            ),
-          ],
+          ),
         ),
       ),
     );
