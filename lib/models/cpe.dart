a25fb80d5ce523b48c3769416d55da4f44a4000d
fiorcode <fiorcode@users.noreply.github.com>
2021-07-03 21:28:06 -0300

    Update cpe.dart

--- lib/models/cpe.dart
+++ lib/models/cpe.dart
@@ -18,6 +18,25 @@ class Cpe {
     this.vulnerabilities,
   });
 
+  String get type {
+    switch (cpe23Uri.split(':')[2]) {
+      case 'h':
+        return 'HARDWARE';
+        break;
+      case 'o':
+        return 'OS/FIRMAWARE';
+        break;
+      default:
+        return 'APP/PROGRAM';
+    }
+  }
+
+  String get trademark => cpe23Uri.split(':')[3].toUpperCase();
+
+  String get model => cpe23Uri.split(':')[4].toUpperCase();
+
+  bool get hasVulns => vulnerabilities.isNotEmpty;
+
   factory Cpe.fromJson(Map<String, dynamic> parsedJson) {
     List<dynamic> _cpeTitles = parsedJson['titles'] as List<dynamic>;
     List<dynamic> _cpeRefs = parsedJson['refs'] as List<dynamic>;
