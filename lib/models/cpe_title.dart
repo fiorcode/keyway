fd814724f38d68eae6029e6b7cc80e6fb8185126
fiorcode <fiorcode@users.noreply.github.com>
2021-07-07 19:54:38 -0300

    Delete cpe_title.dart

--- lib/models/cpe_title.dart
+++
@@ -1,9 +0,0 @@
-class CpeTitle {
-  final String title;
-  final String lang;
-
-  CpeTitle(this.title, this.lang);
-
-  factory CpeTitle.fromJson(Map<String, dynamic> parsedJson) =>
-      CpeTitle(parsedJson['title'], parsedJson['lang']);
-}
