ce29036a1c9be727c180958c5104983bcbd32fc1
fiorcode <fiorcode@users.noreply.github.com>
2021-07-07 19:54:21 -0300

    Create cpe_result.dart

---
+++ lib/models/api/nist/cpe_result.dart
@@ -0,0 +1,28 @@
+import 'package:keyway/models/api/nist/cpe.dart';
+
+class CpeResult {
+  final String dataType;
+  final String feedVersion;
+  final int cpeCount;
+  final String feedTimestamp;
+  final List<Cpe> cpes;
+
+  CpeResult({
+    this.dataType,
+    this.feedVersion,
+    this.cpeCount,
+    this.feedTimestamp,
+    this.cpes,
+  });
+
+  factory CpeResult.fromJson(Map<String, dynamic> parsedJson) {
+    List<dynamic> _cpes = parsedJson['cpes'] as List<dynamic>;
+    return CpeResult(
+      dataType: parsedJson['dataType'],
+      feedVersion: parsedJson['feedVersion'],
+      cpeCount: parsedJson['cpeCount'],
+      feedTimestamp: parsedJson['feedTimestamp'],
+      cpes: _cpes.map((c) => Cpe.fromJson(c)).toList(),
+    );
+  }
+}
