1aebf59fafaf45430632516040c94e599dc2ef3a
fiorcode <fiorcode@users.noreply.github.com>
2021-07-07 19:54:33 -0300

    Delete cpe_ref.dart

--- lib/models/cpe_ref.dart
+++
@@ -1,9 +0,0 @@
-class CpeRef {
-  final String ref;
-  final String type;
-
-  CpeRef(this.ref, this.type);
-
-  factory CpeRef.fromJson(Map<String, dynamic> parsedJson) =>
-      CpeRef(parsedJson['ref'], parsedJson['type']);
-}
