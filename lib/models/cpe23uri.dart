f5bdb74aaf0b3981da1e30edae3e498504660afa
fiorcode <fiorcode@users.noreply.github.com>
2021-07-07 19:54:29 -0300

    Update cpe23uri.dart

--- lib/models/cpe23uri.dart
+++ lib/models/cpe23uri.dart
@@ -1,3 +1,5 @@
+import 'package:keyway/models/api/nist/cpe.dart';
+
 class Cpe23uri {
   int cpe23uriId;
   String value;
@@ -6,6 +8,7 @@ class Cpe23uri {
   String title;
   String ref;
   String refType;
+  String lastTracking;
 
   Cpe23uri({
     this.cpe23uriId,
@@ -15,26 +18,37 @@ class Cpe23uri {
     this.title = '',
     this.ref = '',
     this.refType = '',
+    this.lastTracking = '',
   });
 
+  Cpe23uri.fromCpe(Cpe cpe) {
+    this.value = cpe.cpe23Uri;
+    this.lastModifiedDate = cpe.lastModifiedDate;
+    this.title = cpe.titles.first.title;
+    this.ref = cpe.refs.first.ref;
+    this.refType = cpe.refs.first.type;
+  }
+
   Cpe23uri.fromMap(Map<String, dynamic> map) {
-    cpe23uriId = map['cpe23uri_id'];
-    value = map['value'];
-    deprecated = map['deprecated'];
-    lastModifiedDate = map['last_modified_date'];
-    title = map['title'];
-    ref = map['ref'];
-    refType = map['ref_type'];
+    this.cpe23uriId = map['cpe23uri_id'];
+    this.value = map['value'];
+    this.deprecated = map['deprecated'];
+    this.lastModifiedDate = map['last_modified_date'];
+    this.title = map['title'];
+    this.ref = map['ref'];
+    this.refType = map['ref_type'];
+    this.lastTracking = map['last_tracking'];
   }
 
   Map<String, dynamic> toMap() {
     Map<String, dynamic> map = <String, dynamic>{
-      'value': value,
-      'deprecated': deprecated,
-      'last_modified_date': lastModifiedDate,
-      'title': title,
-      'ref': ref,
-      'ref_type': refType,
+      'value': this.value,
+      'deprecated': this.deprecated,
+      'last_modified_date': this.lastModifiedDate,
+      'title': this.title,
+      'ref': this.ref,
+      'ref_type': this.refType,
+      'last_tracking': this.lastTracking,
     };
     if (cpe23uriId != null) map['cpe23uri_id'] = cpe23uriId;
     return map;
