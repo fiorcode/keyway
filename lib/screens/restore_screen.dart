e66ad31e3f2f8ec256e9371bd611f7655043a585
fiorcode <fiorcode@users.noreply.github.com>
2021-07-20 15:58:31 -0300

    Update restore_screen.dart

--- lib/screens/restore_screen.dart
+++ lib/screens/restore_screen.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/cripto_provider.dart';
-import '../providers/drive_provider.dart';
-import 'items_screen.dart';
-import '../widgets/Cards/backup_status_card.dart';
 
 class RestoreScreen extends StatefulWidget {
   static const routeName = '/restore';
@@ -14,30 +11,16 @@ class RestoreScreen extends StatefulWidget {
 }
 
 class _RestoreScreenState extends State<RestoreScreen> {
-  Future _silently;
   CriptoProvider _cripto;
-  DriveProvider _drive;
   bool _working = false;
+  Future<void> _searchBacks;
 
-  Future<void> _trySilently() async => await _drive.trySignInSilently();
-
-  _restore() async {
-    setState(() => _working = true);
-    DriveProvider drive = Provider.of<DriveProvider>(context, listen: false);
-    drive.downloadDB().then((value) {
-      _cripto.setMasterKey();
-      setState(() => _working = false);
-      Navigator.of(context)
-          .pushNamedAndRemoveUntil(ItemsListScreen.routeName, (route) => false);
-    });
-  }
+  Future<void> _searchBacksAsync() async {}
 
   @override
-  void didChangeDependencies() {
+  void initState() {
     _cripto = Provider.of<CriptoProvider>(context, listen: false);
-    _drive = Provider.of<DriveProvider>(context, listen: true);
-    _silently = _trySilently();
-    super.didChangeDependencies();
+    super.initState();
   }
 
   @override
@@ -47,28 +30,9 @@ class _RestoreScreenState extends State<RestoreScreen> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).backgroundColor,
         iconTheme: IconThemeData(color: Theme.of(context).primaryColor),
-        actions: [
-          _drive.currentUser == null
-              ? TextButton.icon(
-                  icon: Image(
-                    image: AssetImage('assets/google_logo.png'),
-                    height: AppBar().preferredSize.height * .50,
-                  ),
-                  label: Text('Sign in'),
-                  onPressed: _drive.handleSignIn,
-                )
-              : TextButton.icon(
-                  icon: Image(
-                    image: AssetImage('assets/google_logo.png'),
-                    height: AppBar().preferredSize.height * .50,
-                  ),
-                  label: Text('Sign out'),
-                  onPressed: _drive.handleSignOut,
-                ),
-        ],
       ),
       body: FutureBuilder(
-        future: _silently,
+        future: _searchBacks,
         builder: (ctx, snap) {
           switch (snap.connectionState) {
             case ConnectionState.none:
@@ -78,73 +42,20 @@ class _RestoreScreenState extends State<RestoreScreen> {
               return Center(child: CircularProgressIndicator());
               break;
             case ConnectionState.done:
-              return _drive.currentUser == null
-                  ? Center(
-                      child: FittedBox(
-                        fit: BoxFit.fitWidth,
-                        child: Text(
-                          'SIGN IN \nWITH YOUR \nGOOGLE ACCOUNT \nTO \nRESTORE \nYOUR DATA',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 32,
-                            fontWeight: FontWeight.bold,
-                            color: Theme.of(context).primaryColor,
-                          ),
-                        ),
-                      ),
-                    )
-                  : ListView(
-                      padding: EdgeInsets.all(32),
-                      children: [
-                        if (_working)
-                          Center(
-                            child: LinearProgressIndicator(
-                              backgroundColor: Colors.orange[400],
-                            ),
-                          ),
-                        Container(
-                          width: 92,
-                          height: 92,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            image: DecorationImage(
-                                image: NetworkImage(
-                                  _drive.currentUser.photoUrl,
-                                ),
-                                fit: BoxFit.scaleDown),
-                          ),
-                        ),
-                        SizedBox(height: 16),
-                        BackupStatusCard(drive: _drive),
-                        SizedBox(height: 32),
-                        if (_drive.fileFound)
-                          Container(
-                            padding: EdgeInsets.all(8),
-                            width: 256,
-                            child: ElevatedButton.icon(
-                              style: ElevatedButton.styleFrom(
-                                padding: EdgeInsets.symmetric(
-                                    vertical: 8, horizontal: 16),
-                                primary: Colors.white,
-                              ),
-                              onPressed: _restore,
-                              icon: Icon(
-                                Icons.cloud_download,
-                                color: Theme.of(context).primaryColor,
-                                size: 32,
-                              ),
-                              label: Text(
-                                'Restore database',
-                                style: TextStyle(
-                                  color: Theme.of(context).primaryColor,
-                                  fontWeight: FontWeight.w800,
-                                  fontSize: 16,
-                                ),
-                              ),
-                            ),
-                          ),
-                      ],
-                    );
+              return Center(
+                child: FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Text(
+                    'SIGN IN \nWITH YOUR \nGOOGLE ACCOUNT \nTO \nRESTORE \nYOUR DATA',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ),
+              );
               break;
             default:
               return Center(child: Text('default'));
