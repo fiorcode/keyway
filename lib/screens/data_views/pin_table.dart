1691e5853b7fde64951eccdf5b9fc1499161e7d0
fiorcode <fiorcode@users.noreply.github.com>
2021-07-21 19:50:03 -0300

    Create pin_table.dart

---
+++ lib/screens/data_views/pin_table.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:keyway/providers/item_provider.dart';
+import 'package:keyway/helpers/error_helper.dart';
+import 'package:keyway/widgets/empty_items.dart';
+
+class PinTableScreen extends StatefulWidget {
+  static const routeName = '/pin-table';
+
+  @override
+  _PinTableScreenState createState() => _PinTableScreenState();
+}
+
+class _PinTableScreenState extends State<PinTableScreen> {
+  ItemProvider _item;
+  Future _getPins;
+
+  Future<void> _getPinsAsync() async => await _item.fetchPins();
+
+  @override
+  void didChangeDependencies() {
+    _item = Provider.of<ItemProvider>(context);
+    _getPins = _getPinsAsync();
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).backgroundColor,
+        iconTheme: IconThemeData(color: Theme.of(context).primaryColor),
+        title: Text('pin table', style: TextStyle(color: Colors.grey)),
+        centerTitle: true,
+      ),
+      body: FutureBuilder(
+        future: _getPins,
+        builder: (ctx, snap) {
+          switch (snap.connectionState) {
+            case ConnectionState.none:
+              return Center(child: Text('none'));
+            case (ConnectionState.waiting):
+              return Center(child: CircularProgressIndicator());
+            case (ConnectionState.done):
+              if (snap.hasError)
+                return ErrorHelper.errorBody(snap.error);
+              else
+                return _item.pins.length <= 0
+                    ? EmptyItems()
+                    : ListView.separated(
+                        itemCount: _item.pins.length,
+                        itemBuilder: (ctx, i) => ListTile(
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Text('id: '),
+                                  Text(
+                                    _item.pins[i].pinId.toString(),
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text('pin_enc: '),
+                                  Expanded(
+                                    child: Text(
+                                      _item.pins[i].pinEnc,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text('pin_iv: '),
+                                  Expanded(
+                                    child: Text(
+                                      _item.pins[i].pinIv,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text('pin_date: '),
+                                  Expanded(
+                                    child: Text(
+                                      _item.pins[i].pinDate,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text('pin_lapse: '),
+                                  Expanded(
+                                    child: Text(
+                                      _item.pins[i].pinLapse.toString(),
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text('pin_status: '),
+                                  Expanded(
+                                    child: Text(
+                                      _item.pins[i].pinStatus,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        separatorBuilder: (ctx, i) =>
+                            Divider(color: Colors.black),
+                      );
+              break;
+            default:
+              return Center(child: Text('default'));
+          }
+        },
+      ),
+    );
+  }
+}
