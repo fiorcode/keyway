c03a54c8089da5235be0389687064f493bd71e52
fiorcode <fiorcode@users.noreply.github.com>
2021-07-21 19:49:58 -0300

    Create note_table.dart

---
+++ lib/screens/data_views/note_table.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:keyway/providers/item_provider.dart';
+import 'package:keyway/helpers/error_helper.dart';
+import 'package:keyway/widgets/empty_items.dart';
+
+class NoteTableScreen extends StatefulWidget {
+  static const routeName = '/note-table';
+
+  @override
+  _NoteTableScreenState createState() => _NoteTableScreenState();
+}
+
+class _NoteTableScreenState extends State<NoteTableScreen> {
+  ItemProvider _item;
+  Future _getNotes;
+
+  Future<void> _getNotesAsync() async => await _item.fetchNotes();
+
+  @override
+  void didChangeDependencies() {
+    _item = Provider.of<ItemProvider>(context);
+    _getNotes = _getNotesAsync();
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).backgroundColor,
+        iconTheme: IconThemeData(color: Theme.of(context).primaryColor),
+        title: Text('note table', style: TextStyle(color: Colors.grey)),
+        centerTitle: true,
+      ),
+      body: FutureBuilder(
+        future: _getNotes,
+        builder: (ctx, snap) {
+          switch (snap.connectionState) {
+            case ConnectionState.none:
+              return Center(child: Text('none'));
+            case (ConnectionState.waiting):
+              return Center(child: CircularProgressIndicator());
+            case (ConnectionState.done):
+              if (snap.hasError)
+                return ErrorHelper.errorBody(snap.error);
+              else
+                return _item.notes.length <= 0
+                    ? EmptyItems()
+                    : ListView.separated(
+                        itemCount: _item.notes.length,
+                        itemBuilder: (ctx, i) => ListTile(
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Text('id: '),
+                                  Text(
+                                    _item.notes[i].noteId.toString(),
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text('note_enc: '),
+                                  Expanded(
+                                    child: Text(
+                                      _item.notes[i].noteEnc,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text('note_iv: '),
+                                  Expanded(
+                                    child: Text(
+                                      _item.notes[i].noteIv,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        separatorBuilder: (ctx, i) =>
+                            Divider(color: Colors.black),
+                      );
+              break;
+            default:
+              return Center(child: Text('default'));
+          }
+        },
+      ),
+    );
+  }
+}
