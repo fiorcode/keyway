d221a3e34d88a76c5a797cc1f7d3e35b498f40f1
fiorcode <fiorcode@users.noreply.github.com>
2021-07-26 21:22:59 -0300

    Update backup_restore_screen.dart

--- lib/screens/backup_restore_screen.dart
+++ lib/screens/backup_restore_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 import 'package:external_path/external_path.dart';
+import 'package:keyway/helpers/db_helper.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 // import '../widgets/loading_scaffold.dart';
@@ -15,31 +16,20 @@ class BackupRestoreScreen extends StatefulWidget {
 
 class _BackupRestoreScreenState extends State<BackupRestoreScreen> {
   Future<void> getPath_1() async {
-    var status = await Permission.storage.status;
+    PermissionStatus status = await Permission.manageExternalStorage.status;
     if (status.isDenied) {
-      if (await Permission.storage.request().isGranted) {
-        var path = await ExternalPath.getExternalStorageDirectories();
-        // print(path);
-        Directory _dir = Directory(path[0]);
-        List<FileSystemEntity> _list = _dir.listSync();
-        _list.forEach((f) {
-          print(f.path);
-        });
-        // _dir = Directory(path[1]);
-        // _list = _dir.listSync();
-        // _list.forEach((f) {
-        //   print(f.path);
-        // });
-      }
-    } else {
-      var path = await ExternalPath.getExternalStorageDirectories();
-      // print(path);
-      Directory _dir = Directory('/storage/emulated/0/Download');
+      if (!(await Permission.manageExternalStorage.request().isGranted)) return;
+    }
+    final _dbPath = await DBHelper.dbPath();
+    final _localDB = File('$_dbPath/kw.db');
+    var path = await ExternalPath.getExternalStorageDirectories();
+    Directory(path[1] + '/keyway/backups').create().then((_dir) {
+      _localDB.copySync('${_dir.path}/kw_baskup.db');
       List<FileSystemEntity> _list = _dir.listSync();
       _list.forEach((f) {
         print(f.path);
       });
-    }
+    });
   }
 
   @override
