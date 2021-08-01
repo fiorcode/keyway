c5973745a6050134d04ad05d78e95c965be1902e
fiorcode <fiorcode@users.noreply.github.com>
2021-07-31 23:15:46 -0300

    Delete path_helper.dart

--- lib/helpers/path_helper.dart
+++
@@ -1,6 +0,0 @@
-import 'package:external_path/external_path.dart';
-
-class PathHelper {
-  static Future<List<String>> externalPaths() async =>
-      await ExternalPath.getExternalStorageDirectories();
-}
