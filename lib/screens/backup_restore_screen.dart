c27c804d4640aa679e79215651affeed9cc82762
fiorcode <fiorcode@users.noreply.github.com>
2021-07-31 23:15:56 -0300

    Update backup_restore_screen.dart

--- lib/screens/backup_restore_screen.dart
+++ lib/screens/backup_restore_screen.dart
@@ -1,12 +1,9 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
+// import 'package:permission_handler/permission_handler.dart';
 
-import 'package:external_path/external_path.dart';
-import 'package:keyway/helpers/db_helper.dart';
-import 'package:keyway/widgets/loading_scaffold.dart';
-import 'package:permission_handler/permission_handler.dart';
-
-// import '../widgets/loading_scaffold.dart';
+import '../helpers/storage_helper.dart';
+import '../models/depot.dart';
+import '../widgets/loading_scaffold.dart';
 
 class BackupRestoreScreen extends StatefulWidget {
   static const routeName = '/backup-restore';
@@ -16,82 +13,28 @@ class BackupRestoreScreen extends StatefulWidget {
 }
 
 class _BackupRestoreScreenState extends State<BackupRestoreScreen> {
-  Future<List<Widget>> _getStorages;
+  Future<List<Depot>> _getDepots;
+  List<Depot> _depots;
 
-  // Future<void> getPath_1() async {
-  //   final _dbPath = await DBHelper.dbPath();
-  //   final _localDB = File('$_dbPath/kw.db');
-  //   var path = await ExternalPath.getExternalStorageDirectories();
-  //   Directory(path[1] + '/keyway/backups').create().then((_dir) {
-  //     _localDB.copySync('${_dir.path}/kw_baskup.db');
-  //     List<FileSystemEntity> _list = _dir.listSync();
-  //     _list.forEach((f) {
-  //       print(f.path);
-  //     });
-  //   });
+  // PermissionStatus _status = await Permission.manageExternalStorage.status;
+  // if (_status.isDenied) {
+  //   if (!(await Permission.manageExternalStorage.request().isGranted))
+  //     return _cards;
   // }
 
-  Future<List<Widget>> _getStoragesAsync() async => await _options();
-
-  Future<List<Widget>> _options() async {
-    List<Widget> _cards = <Widget>[];
-    // PermissionStatus _status = await Permission.manageExternalStorage.status;
-    // if (_status.isDenied) {
-    //   if (!(await Permission.manageExternalStorage.request().isGranted))
-    //     return _cards;
-    // }
-    List<String> _paths = await ExternalPath.getExternalStorageDirectories();
-    _paths.forEach((p) {
-      _cards.add(
-        Card(
-          clipBehavior: Clip.antiAlias,
-          elevation: 8,
-          shadowColor: Colors.grey,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16.0),
-            side: BorderSide(width: 3, color: Colors.grey),
-          ),
-          child: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Colors.grey[100],
-                  Colors.grey[300],
-                  Colors.grey[600],
-                ],
-              ),
-            ),
-            height: 128,
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                p.split('/').last == '0'
-                    ? Icon(Icons.phone_iphone, size: 48)
-                    : Icon(Icons.sd_card, size: 48),
-                Text(p),
-              ],
-            ),
-          ),
-        ),
-      );
-    });
-    return _cards;
-  }
+  Future<List<Depot>> _getDepotsAsync() async =>
+      await StorageHelper.getDepots();
 
   @override
   void initState() {
-    _getStorages = _getStoragesAsync();
+    _getDepots = _getDepotsAsync();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: _getStorages,
+        future: _getDepots,
         builder: (ctx, snap) {
           switch (snap.connectionState) {
             case ConnectionState.active:
@@ -100,7 +43,8 @@ class _BackupRestoreScreenState extends State<BackupRestoreScreen> {
             case ConnectionState.done:
               if (snap.hasError)
                 return Text(snap.error);
-              else
+              else {
+                _depots = snap.data;
                 return Scaffold(
                   backgroundColor: Theme.of(context).backgroundColor,
                   appBar: AppBar(
@@ -121,11 +65,52 @@ class _BackupRestoreScreenState extends State<BackupRestoreScreen> {
                           ),
                         ),
                         SizedBox(height: 16.0),
-                        ...snap.data
+                        ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: _depots.length,
+                            itemBuilder: (ctx, i) {
+                              return Card(
+                                clipBehavior: Clip.antiAlias,
+                                elevation: 8,
+                                shadowColor: Colors.grey,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16.0),
+                                  side:
+                                      BorderSide(width: 3, color: Colors.grey),
+                                ),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    gradient: LinearGradient(
+                                      begin: Alignment.topLeft,
+                                      end: Alignment.bottomRight,
+                                      colors: [
+                                        Colors.grey[100],
+                                        Colors.grey[300],
+                                        Colors.grey[600],
+                                      ],
+                                    ),
+                                  ),
+                                  height: 128,
+                                  width: double.infinity,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      _depots[i].icon,
+                                      Text(_depots[i].title),
+                                      Text(_depots[i].path),
+                                      Text(_depots.length.toString()),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            }),
                       ],
                     ),
                   ),
                 );
+              }
               break;
             default:
               return Text('default');
