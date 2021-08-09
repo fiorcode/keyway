2a715418533f4207bcec78622ad1786059090346
fiorcode <fiorcode@users.noreply.github.com>
2021-08-09 17:51:44 -0300

    Update storage_helper.dart

--- lib/helpers/storage_helper.dart
+++ lib/helpers/storage_helper.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:external_path/external_path.dart';
+import 'package:keyway/helpers/db_helper.dart';
 
 import '../models/depot.dart';
 
@@ -9,13 +10,14 @@ class StorageHelper {
       await ExternalPath.getExternalStorageDirectories();
 
   static Future<String> sdCardPath() async {
+    String _path = '';
     List<String> _paths = await _externalPaths();
     if (_paths.isNotEmpty) {
       _paths.forEach((path) {
-        if (path.split('/').last.contains('-')) return path;
+        if (path.split('/').last.contains('-')) _path = path;
       });
     }
-    return '';
+    return _path;
   }
 
   static Future<String> devicePath() async {
@@ -27,6 +29,24 @@ class StorageHelper {
       });
     }
     return _path;
+  }
+
+  static Future<bool> backupToDevice() async {
+    String _path = await devicePath();
+    if (_path.isNotEmpty) {
+      return await DBHelper.createBackup(_path);
+    } else {
+      return false;
+    }
+  }
+
+  static Future<bool> backupToSdCard() async {
+    String _path = await sdCardPath();
+    if (_path.isNotEmpty) {
+      return await DBHelper.createBackup(_path);
+    } else {
+      return false;
+    }
   }
 
   static Future<File> getDeviceBackup() async {
