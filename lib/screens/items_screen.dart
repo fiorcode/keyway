6a1e4ba6bf139817fd4d29e254f397c1c2dd3159
fiorcode <fiorcode@users.noreply.github.com>
2021-08-23 23:18:56 -0300

    Update items_screen.dart

--- lib/screens/items_screen.dart
+++ lib/screens/items_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:keyway/widgets/loading_scaffold.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/cripto_provider.dart';
 import '../providers/item_provider.dart';
+import 'package:keyway/models/tag.dart';
 import '../screens/item_add_screen.dart';
 import '../screens/dashboard_screen.dart';
 import '../widgets/card/item_locked_card.dart';
@@ -11,6 +11,8 @@ import '../widgets/card/item_unlocked_card.dart';
 import '../widgets/unlock_container.dart';
 import '../widgets/empty_items.dart';
 import '../widgets/text_field/search_bar_text_field.dart';
+import '../widgets/item_filter_list.dart';
+import '../widgets/loading_scaffold.dart';
 
 class ItemsListScreen extends StatefulWidget {
   static const routeName = '/items';
@@ -23,21 +25,17 @@ class _ItemsListScreenState extends State<ItemsListScreen> {
   CriptoProvider _cripto;
   ItemProvider _item;
   Future<void> _getItems;
+  List<Tag> _tags = <Tag>[];
 
   TextEditingController _searchCtrler = TextEditingController();
   FocusNode _searchFN = FocusNode();
 
   bool _unlocking = false;
   bool _searching = false;
+  bool _deleted = false;
+  bool _oldPassword = false;
 
   _lockSwitch() => setState(() => _unlocking = !_unlocking);
-
-  Future<void> _getItemsAsync() => _item.fetchItems(_searchCtrler.text);
-
-  void _onReturn() {
-    _getItems = _getItemsAsync();
-    setState(() {});
-  }
 
   void _searchSwitch() {
     setState(() {
@@ -45,6 +43,44 @@ class _ItemsListScreenState extends State<ItemsListScreen> {
       _searching ? _searchFN.requestFocus() : _searchFN.unfocus();
       if (!_searching) _clearSearch();
     });
+  }
+
+  void _deletedSwitch() {
+    _deleted = !_deleted;
+    _deleted
+        ? _getItems = _getItemsDeletedAsync()
+        : _getItems = _getItemsAsync();
+    setState(() {});
+  }
+
+  void _oldPasswordSwitch() {
+    if (_deleted) _deleted = false;
+    _oldPassword = !_oldPassword;
+    _oldPassword
+        ? _getItems = _getItemsDeletedAsync()
+        : _getItems = _getItemsAsync();
+    setState(() {});
+  }
+
+  void _tagsSwitch() {
+    if (_tags.isEmpty)
+      _getItems = _getItemsAsync();
+    else
+      _getItems = _getItemsWithTags();
+    if (_deleted) _deleted = false;
+    if (_oldPassword) _oldPassword = false;
+    setState(() {});
+  }
+
+  Future<void> _getItemsAsync() => _item.fetchItems(_searchCtrler.text);
+
+  Future<void> _getItemsDeletedAsync() => _item.fetchItemsDeleted();
+
+  Future<void> _getItemsWithTags() => _item.fetchItemsWithTags(_tags);
+
+  void _onReturn() {
+    _getItems = _getItemsAsync();
+    setState(() {});
   }
 
   void _clearSearch() {
@@ -110,6 +146,8 @@ class _ItemsListScreenState extends State<ItemsListScreen> {
 
   @override
   Widget build(BuildContext context) {
+    Color _primary = Theme.of(context).primaryColor;
+    Color _back = Theme.of(context).backgroundColor;
     return FutureBuilder(
         future: _getItems,
         builder: (ctx, snap) {
@@ -122,11 +160,10 @@ class _ItemsListScreenState extends State<ItemsListScreen> {
                 return Center(child: Image.asset("assets/error.png"));
               else {
                 return Scaffold(
-                  backgroundColor: Theme.of(context).backgroundColor,
+                  backgroundColor: _back,
                   appBar: AppBar(
-                    backgroundColor: Theme.of(context).backgroundColor,
-                    iconTheme:
-                        IconThemeData(color: Theme.of(context).primaryColor),
+                    backgroundColor: _back,
+                    iconTheme: IconThemeData(color: _primary),
                     centerTitle: true,
                     leading: IconButton(
                       icon: Icon(Icons.menu),
@@ -144,24 +181,39 @@ class _ItemsListScreenState extends State<ItemsListScreen> {
                         onPressed: _goToAlpha,
                       )
                     ],
-                    actionsIconTheme:
-                        IconThemeData(color: Theme.of(context).primaryColor),
+                    actionsIconTheme: IconThemeData(color: _primary),
                   ),
                   body: Stack(
                     children: [
-                      _item.items.length <= 0
+                      snap.data.length <= 0
                           ? EmptyItems()
-                          : ListView.builder(
-                              padding: EdgeInsets.all(12.0),
-                              itemCount: _item.items.length,
-                              itemBuilder: (ctx, i) {
-                                return _cripto.locked
-                                    ? ItemLockedCard(item: _item.items[i])
-                                    : ItemUnlockedCard(
-                                        item: _item.items[i],
-                                        onReturn: _onReturn,
-                                      );
-                              },
+                          : SingleChildScrollView(
+                              child: Column(
+                                children: [
+                                  ItemFilterList(
+                                    _deleted,
+                                    _deletedSwitch,
+                                    _oldPassword,
+                                    _oldPasswordSwitch,
+                                  ),
+                                  // TagsCard(),
+                                  ListView.builder(
+                                    padding: EdgeInsets.all(12.0),
+                                    physics:
+                                        const NeverScrollableScrollPhysics(),
+                                    shrinkWrap: true,
+                                    itemCount: snap.data.length,
+                                    itemBuilder: (ctx, i) {
+                                      return _cripto.locked
+                                          ? ItemLockedCard(item: snap.data[i])
+                                          : ItemUnlockedCard(
+                                              item: snap.data[i],
+                                              onReturn: _onReturn,
+                                            );
+                                    },
+                                  ),
+                                ],
+                              ),
                             ),
                       if (_unlocking && _cripto.locked)
                         UnlockContainer(_lockSwitch),
