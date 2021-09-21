ae13f5ea26dfd51c7e5a9858dcce385b62651c4f
fiorcode <fiorcode@users.noreply.github.com>
2021-09-21 20:37:33 -0300

    Update item_view_container.dart

--- lib/widgets/item_view_container.dart
+++ lib/widgets/item_view_container.dart
@@ -3,12 +3,12 @@ import 'package:keyway/helpers/date_helper.dart';
 
 class ItemViewContainer extends StatelessWidget {
   const ItemViewContainer(this.title, this.content,
-      {this.date = '', this.lapse = 0});
+      {this.left = '', this.right = 0});
 
   final String title;
   final String content;
-  final String date;
-  final int lapse;
+  final String left;
+  final int right;
 
   @override
   Widget build(BuildContext context) {
@@ -28,64 +28,87 @@ class ItemViewContainer extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: date.isNotEmpty
-                ? MainAxisAlignment.spaceBetween
-                : MainAxisAlignment.center,
-            children: [
-              date.isNotEmpty
-                  ? Expanded(
-                      child: Container(
-                        padding: EdgeInsets.symmetric(
-                          vertical: 2,
-                          horizontal: 4,
-                        ),
-                        color: Colors.black,
-                        child: FittedBox(
-                          fit: BoxFit.fitWidth,
-                          child: Text(
-                            DateHelper.ddMMyyyy(date),
-                            textAlign: TextAlign.center,
-                            style: TextStyle(fontSize: 18, color: Colors.white),
+          Container(
+            height: 24,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: left.isNotEmpty
+                  ? MainAxisAlignment.spaceBetween
+                  : MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: left.isNotEmpty
+                      ? Container(
+                          padding: EdgeInsets.all(2),
+                          color: Colors.black,
+                          child: FittedBox(
+                            fit: BoxFit.fitHeight,
+                            clipBehavior: Clip.hardEdge,
+                            child: Text(
+                              this.title == 'address'
+                                  ? this.left
+                                  : DateHelper.ddMMyyyy(left),
+                              maxLines: 1,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.white),
+                            ),
                           ),
-                        ),
+                        )
+                      : Container(),
+                ),
+                Container(width: 32),
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(2),
+                    color: Colors.black,
+                    child: FittedBox(
+                      fit: BoxFit.fitHeight,
+                      clipBehavior: Clip.hardEdge,
+                      child: Text(
+                        this.title,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white),
                       ),
-                    )
-                  : Expanded(child: Container()),
-              Container(width: 32),
-              Expanded(
-                child: Container(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 2,
-                    horizontal: 4,
-                  ),
-                  color: Colors.black,
-                  child: Text(
-                    this.title,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
                   ),
                 ),
-              ),
-              Container(width: 32),
-              DateHelper.expired(date, lapse)
-                  ? Expanded(
-                      child: Container(
-                        padding: EdgeInsets.symmetric(
-                          vertical: 2,
-                          horizontal: 4,
-                        ),
-                        color: Colors.red,
+                Container(width: 32),
+                if (this.title == 'address')
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(2),
+                      color: Colors.black,
+                      child: FittedBox(
+                        fit: BoxFit.fitHeight,
+                        clipBehavior: Clip.hardEdge,
                         child: Text(
-                          'Expired',
+                          this.right.toString(),
                           textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 18, color: Colors.white),
+                          style: TextStyle(color: Colors.white),
                         ),
                       ),
-                    )
-                  : Expanded(child: Container()),
-            ],
+                    ),
+                  ),
+                if (this.title != 'address')
+                  Expanded(
+                    child: DateHelper.expired(left, right)
+                        ? Container(
+                            padding: EdgeInsets.all(2),
+                            color: Colors.red,
+                            child: FittedBox(
+                              fit: BoxFit.fitHeight,
+                              clipBehavior: Clip.hardEdge,
+                              child: Text(
+                                'expired',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          )
+                        : Container(),
+                  )
+              ],
+            ),
           ),
           Expanded(
             child: Center(
