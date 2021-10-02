0f2da7e2e3806da1e1328f87e25a2daf44e8bd36
fiorcode <fiorcode@users.noreply.github.com>
2021-10-02 15:05:15 -0300

    Update item_view_screen.dart

--- lib/screens/item_view_screen.dart
+++ lib/screens/item_view_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:keyway/helpers/error_helper.dart';
 import 'package:provider/provider.dart';
 
 import 'item_edit_screen.dart';
@@ -26,17 +27,23 @@ class _ItemViewScreenState extends State<ItemViewScreen> {
   CriptoProvider _cripto;
   ItemProvider _items;
   Item _i;
-  bool _loading = false;
-  Future<void> _getPasswords;
+  Future<void> _loadItem;
 
   Future<void> _onReturn(Item iUpdated) async {
     if (iUpdated != null) setState(() => _i = iUpdated);
     //if (changed) await _getItem();
   }
 
-  Future<void> _getPasswordsAsync() async {
+  // Future<void> _getPasswordsAsync() async {
+  //   _items = Provider.of<ItemProvider>(context, listen: false);
+  //   _getPasswords = _items.loadPasswords(_i);
+  // }
+
+  Future<void> _loadItemAsync() async {
     _items = Provider.of<ItemProvider>(context, listen: false);
-    _getPasswords = _items.loadPasswords(_i);
+    await _items.loadPasswords(_i);
+    _cripto = Provider.of<CriptoProvider>(context, listen: false);
+    await _cripto.decryptItem(_i);
   }
 
   void _goToEditItem() {
@@ -70,67 +77,64 @@ class _ItemViewScreenState extends State<ItemViewScreen> {
   @override
   void initState() {
     _i = widget.item;
-    _getPasswords = _getPasswordsAsync();
+    _loadItem = _loadItemAsync();
+    // _getPasswords = _getPasswordsAsync();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    _cripto = Provider.of<CriptoProvider>(context, listen: false);
-    if (_loading)
-      return LoadingScaffold();
-    else
-      return Scaffold(
-        backgroundColor: Theme.of(context).backgroundColor,
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).backgroundColor,
-          iconTheme: IconThemeData(color: Theme.of(context).primaryColor),
-          centerTitle: true,
-          title: Text(
-            _i.title,
-            style: TextStyle(
-              fontSize: 32,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
-          ),
-          actions: [
-            IconButton(icon: Icon(Icons.edit), onPressed: _goToEditItem)
-          ],
-          actionsIconTheme:
-              IconThemeData(color: Theme.of(context).primaryColor),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                if (_i.username != null)
-                  ItemViewContainer(
-                    'username',
-                    _cripto.decryptUsername(_i.username),
+    return FutureBuilder(
+        future: _loadItem,
+        builder: (ctx, snap) {
+          switch (snap.connectionState) {
+            case ConnectionState.waiting:
+              return LoadingScaffold();
+              break;
+            case ConnectionState.done:
+              if (snap.hasError)
+                return ErrorBody(snap.error);
+              else {
+                return Scaffold(
+                  backgroundColor: Theme.of(context).backgroundColor,
+                  appBar: AppBar(
+                    backgroundColor: Theme.of(context).backgroundColor,
+                    iconTheme:
+                        IconThemeData(color: Theme.of(context).primaryColor),
+                    centerTitle: true,
+                    title: Text(
+                      _i.title,
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    actions: [
+                      IconButton(
+                          icon: Icon(Icons.edit), onPressed: _goToEditItem)
+                    ],
+                    actionsIconTheme:
+                        IconThemeData(color: Theme.of(context).primaryColor),
                   ),
-                if (_i.password != null)
-                  ItemViewContainer(
-                    'password',
-                    _cripto.decryptPassword(_i.password),
-                    left: _i.itemPassword.passwordDate,
-                    right: _i.itemPassword.passwordLapse,
-                  ),
-                FutureBuilder(
-                  future: _getPasswords,
-                  builder: (ctx, snap) {
-                    switch (snap.connectionState) {
-                      case ConnectionState.waiting:
-                        return LinearProgressIndicator();
-                        break;
-                      case ConnectionState.done:
-                        if (snap.hasError)
-                          return Text(snap.error);
-                        else {
-                          if (_i.itemPasswords.isNotEmpty) {
-                            return Expanded(
+                  body: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          if (_i.username != null)
+                            ItemViewContainer(
+                                'username', _i.username.usernameDec),
+                          if (_i.password != null)
+                            ItemViewContainer(
+                              'password',
+                              _i.password.passwordDec,
+                              left: _i.itemPassword.passwordDate,
+                              right: _i.itemPassword.passwordLapse,
+                            ),
+                          if (_i.itemPasswords.isNotEmpty)
+                            Expanded(
                               child: Center(
                                 child: TextButton(
                                   onPressed: _goToPasswordHistory,
@@ -143,148 +147,152 @@ class _ItemViewScreenState extends State<ItemViewScreen> {
                                   ),
                                 ),
                               ),
-                            );
-                          } else {
-                            return SizedBox(height: 0);
-                          }
-                        }
-                        break;
-                      default:
-                        return LinearProgressIndicator();
-                    }
-                  },
-                ),
-                if (_i.pin != null)
-                  ItemViewContainer(
-                    'pin',
-                    _cripto.decryptPin(_i.pin),
-                    left: _i.pin.pinDate,
-                    right: _i.pin.pinLapse,
-                  ),
-                if (_i.note != null)
-                  ItemViewContainer(
-                    'note',
-                    _cripto.decryptNote(_i.note),
-                  ),
-                if (_i.address != null)
-                  ItemViewContainer(
-                    'address',
-                    _cripto.decryptAddress(_i.address),
-                    left: _i.address.addressProtocol,
-                    right: _i.address.addressPort,
-                  ),
-                if (_i.product != null)
-                  Container(
-                    width: double.infinity,
-                    height: 160,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 3.0,
+                            ),
+                          // if (_i.itemPasswords.isEmpty) SizedBox(height: 0),
+                          if (_i.pin != null)
+                            ItemViewContainer(
+                              'pin',
+                              _i.pin.pinDec,
+                              left: _i.pin.pinDate,
+                              right: _i.pin.pinLapse,
+                            ),
+                          if (_i.note != null)
+                            ItemViewContainer('note', _i.note.noteDec),
+                          if (_i.address != null)
+                            ItemViewContainer(
+                              'address',
+                              _i.address.addressDec,
+                              left: _i.address.addressProtocol,
+                              right: _i.address.addressPort,
+                            ),
+                          if (_i.product != null)
+                            Container(
+                              width: double.infinity,
+                              height: 160,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.black,
+                                  width: 3.0,
+                                ),
+                                borderRadius: BorderRadius.circular(8),
+                                color: Colors.white,
+                              ),
+                              margin: EdgeInsets.symmetric(vertical: 8),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    padding: EdgeInsets.symmetric(
+                                      vertical: 2,
+                                      horizontal: 4,
+                                    ),
+                                    color: Colors.black,
+                                    child: Text(
+                                      'product',
+                                      style: TextStyle(
+                                          fontSize: 18, color: Colors.white),
+                                    ),
+                                  ),
+                                  if (_i.product.productTrademark.isNotEmpty)
+                                    Expanded(
+                                      child: Center(
+                                        child: Text(
+                                          'trademark',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.grey,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  if (_i.product.productTrademark.isNotEmpty)
+                                    Expanded(
+                                      child: Center(
+                                        child: Text(
+                                          _i.product.productTrademark[0]
+                                                  .toUpperCase() +
+                                              _i.product.productTrademark
+                                                  .substring(1),
+                                          style: TextStyle(
+                                            fontSize: 22,
+                                            fontWeight: FontWeight.w300,
+                                            color:
+                                                Theme.of(context).primaryColor,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  if (_i.product.productModel.isNotEmpty)
+                                    Expanded(
+                                      child: Center(
+                                        child: Text(
+                                          'model',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.grey,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  if (_i.product.productModel.isNotEmpty)
+                                    Expanded(
+                                      child: Center(
+                                        child: Text(
+                                          _i.product.productModel[0]
+                                                  .toUpperCase() +
+                                              _i.product.productModel
+                                                  .substring(1),
+                                          style: TextStyle(
+                                            fontSize: 22,
+                                            fontWeight: FontWeight.w300,
+                                            color:
+                                                Theme.of(context).primaryColor,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                ],
+                              ),
+                            ),
+                          _i.deleted
+                              ? ElevatedButton.icon(
+                                  style: ElevatedButton.styleFrom(
+                                      primary: Colors.white),
+                                  icon: Icon(
+                                    Icons.restore_from_trash,
+                                    color: Colors.grey,
+                                  ),
+                                  label: Text(
+                                    'RESTORE',
+                                    style: TextStyle(color: Colors.grey),
+                                  ),
+                                  onPressed: _deleteItem,
+                                )
+                              : Container(
+                                  width: double.infinity,
+                                  child: ElevatedButton.icon(
+                                    style: ElevatedButton.styleFrom(
+                                        primary: Colors.red),
+                                    icon:
+                                        Icon(Icons.delete, color: Colors.white),
+                                    label: Text(
+                                      'DELETE ITEM',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                    onPressed: _deleteItem,
+                                  ),
+                                ),
+                        ],
                       ),
-                      borderRadius: BorderRadius.circular(8),
-                      color: Colors.white,
-                    ),
-                    margin: EdgeInsets.symmetric(vertical: 8),
-                    child: Column(
-                      children: [
-                        Container(
-                          padding: EdgeInsets.symmetric(
-                            vertical: 2,
-                            horizontal: 4,
-                          ),
-                          color: Colors.black,
-                          child: Text(
-                            'product',
-                            style: TextStyle(fontSize: 18, color: Colors.white),
-                          ),
-                        ),
-                        if (_i.product.productTrademark.isNotEmpty)
-                          Expanded(
-                            child: Center(
-                              child: Text(
-                                'trademark',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            ),
-                          ),
-                        if (_i.product.productTrademark.isNotEmpty)
-                          Expanded(
-                            child: Center(
-                              child: Text(
-                                _i.product.productTrademark[0].toUpperCase() +
-                                    _i.product.productTrademark.substring(1),
-                                style: TextStyle(
-                                  fontSize: 22,
-                                  fontWeight: FontWeight.w300,
-                                  color: Theme.of(context).primaryColor,
-                                ),
-                              ),
-                            ),
-                          ),
-                        if (_i.product.productModel.isNotEmpty)
-                          Expanded(
-                            child: Center(
-                              child: Text(
-                                'model',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            ),
-                          ),
-                        if (_i.product.productModel.isNotEmpty)
-                          Expanded(
-                            child: Center(
-                              child: Text(
-                                _i.product.productModel[0].toUpperCase() +
-                                    _i.product.productModel.substring(1),
-                                style: TextStyle(
-                                  fontSize: 22,
-                                  fontWeight: FontWeight.w300,
-                                  color: Theme.of(context).primaryColor,
-                                ),
-                              ),
-                            ),
-                          ),
-                      ],
                     ),
                   ),
-                _i.deleted
-                    ? ElevatedButton.icon(
-                        style: ElevatedButton.styleFrom(primary: Colors.white),
-                        icon: Icon(
-                          Icons.restore_from_trash,
-                          color: Colors.grey,
-                        ),
-                        label: Text(
-                          'RESTORE',
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                        onPressed: _deleteItem,
-                      )
-                    : Container(
-                        width: double.infinity,
-                        child: ElevatedButton.icon(
-                          style: ElevatedButton.styleFrom(primary: Colors.red),
-                          icon: Icon(Icons.delete, color: Colors.white),
-                          label: Text(
-                            'DELETE ITEM',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          onPressed: _deleteItem,
-                        ),
-                      ),
-              ],
-            ),
-          ),
-        ),
-      );
+                );
+              }
+              break;
+            default:
+              return LoadingScaffold();
+          }
+        });
   }
 }
