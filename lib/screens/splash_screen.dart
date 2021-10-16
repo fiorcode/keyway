282ee6da8a2707aa2f3867295383086ca66ba55b
fiorcode <fiorcode@users.noreply.github.com>
2021-10-16 14:51:58 -0300

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:keyway/providers/cripto_provider.dart';
-import 'package:keyway/providers/item_provider.dart';
+// import 'package:keyway/providers/item_provider.dart';
 import 'items_screen.dart';
-// import 'set_password_screen.dart';
+import 'set_password_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   static const routeName = '/splash';
@@ -21,12 +21,12 @@ class _SplashScreenState extends State<SplashScreen> {
     if (await _cripto.isMasterKey()) {
       Navigator.of(context).pushReplacementNamed(ItemsListScreen.routeName);
     } else {
-      if (await CriptoProvider.initialSetup('Qwe123!')) {
-        await _cripto.unlock('Qwe123!');
-        await ItemProvider().mockData();
-        Navigator.of(context).pushReplacementNamed(ItemsListScreen.routeName);
-      }
-      // Navigator.of(context).pushReplacementNamed(SetPasswordScreen.routeName);
+      // if (await CriptoProvider.initialSetup('Qwe123!')) {
+      //   await _cripto.unlock('Qwe123!');
+      //   await ItemProvider().mockData();
+      //   Navigator.of(context).pushReplacementNamed(ItemsListScreen.routeName);
+      // }
+      Navigator.of(context).pushReplacementNamed(SetPasswordScreen.routeName);
     }
   }
 
