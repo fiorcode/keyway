911bf7349d08542a81f9ca8aa6246847d3e1d86d
fiorcode <fiorcode@users.noreply.github.com>
2021-11-13 19:35:20 -0300

    Update item_locked_card.dart

--- lib/widgets/card/item_locked_card.dart
+++ lib/widgets/card/item_locked_card.dart
@@ -1,15 +1,27 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:keyway/helpers/password_helper.dart';
+import 'package:keyway/providers/item_provider.dart';
+import 'package:provider/provider.dart';
 
 import '../../helpers/date_helper.dart';
 import '../../models/item.dart';
 
-class ItemLockedCard extends StatelessWidget {
+class ItemLockedCard extends StatefulWidget {
   const ItemLockedCard({Key? key, this.item}) : super(key: key);
 
   final Item? item;
 
+  @override
+  _ItemLockedCardState createState() => _ItemLockedCardState();
+}
+
+class _ItemLockedCardState extends State<ItemLockedCard> {
+  bool _pin = false;
+
   Color _setAvatarLetterColor() {
-    Color _color = Color(item!.avatarColor!);
+    Color _color = Color(widget.item!.avatarColor!);
     double bgDelta =
         _color.red * 0.299 + _color.green * 0.587 + _color.blue * 0.114;
     return (255 - bgDelta > 105)
@@ -30,6 +42,25 @@ class ItemLockedCard extends StatelessWidget {
     );
   }
 
+  void _passwordPin(BuildContext context) async {
+    _pin = !_pin;
+    if (_pin)
+      widget.item!.title = Random.secure().nextInt(9999).toString();
+    else {
+      widget.item!.title = (await PasswordHelper.dicePassword()).password!;
+    }
+    Provider.of<ItemProvider>(context, listen: false).updateItem(widget.item!);
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    if (widget.item!.cleartext) {
+      if (!widget.item!.title.contains(RegExp(r'[a-zA-Z]'))) _pin = true;
+    }
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -43,13 +74,13 @@ class ItemLockedCard extends StatelessWidget {
         contentPadding: EdgeInsets.all(4),
         leading: CircleAvatar(
           radius: 24,
-          backgroundColor: item!.avatarColor != null
-              ? Color(item!.avatarColor!).withAlpha(192)
+          backgroundColor: widget.item!.avatarColor != null
+              ? Color(widget.item!.avatarColor!).withAlpha(192)
               : Colors.grey,
-          child: item!.cleartext
+          child: widget.item!.cleartext
               ? Icon(Icons.flash_on)
               : Text(
-                  item!.title.substring(0, 1).toUpperCase(),
+                  widget.item!.title.substring(0, 1).toUpperCase(),
                   style: TextStyle(
                     fontSize: 24,
                     fontWeight: FontWeight.bold,
@@ -63,7 +94,7 @@ class ItemLockedCard extends StatelessWidget {
             FittedBox(
               fit: BoxFit.fitWidth,
               child: Text(
-                item!.title,
+                widget.item!.title,
                 maxLines: 1,
                 softWrap: true,
                 style: TextStyle(
@@ -73,13 +104,28 @@ class ItemLockedCard extends StatelessWidget {
                 ),
               ),
             ),
-            Text(DateHelper.ddMMyyHm(item!.date)),
+            Text(DateHelper.ddMMyyHm(widget.item!.date)),
           ],
         ),
         onTap: () => _onTap(context),
         trailing: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12),
-          child: Icon(Icons.lock, color: Colors.red),
+          padding: const EdgeInsets.all(4.0),
+          child: widget.item!.cleartext
+              ? SizedBox(
+                  height: 48,
+                  width: 48,
+                  child: FloatingActionButton(
+                    backgroundColor: Colors.grey[100],
+                    child: Icon(
+                      _pin ? Icons.password : Icons.pin,
+                      color: Colors.grey,
+                      size: 24,
+                    ),
+                    heroTag: null,
+                    onPressed: () => _passwordPin(context),
+                  ),
+                )
+              : Icon(Icons.lock, color: Colors.red),
         ),
       ),
     );
