f36b94cb29c9eed2944b5074a95dc060fa155571
fiorcode <fiorcode@users.noreply.github.com>
2021-12-06 19:36:06 -0300

    Create notification_service.dart

---
+++ lib/services/notification_service.dart
@@ -0,0 +1,87 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
+class NotificationService {
+  //Singleton pattern
+  static final NotificationService _notificationService =
+      NotificationService._internal();
+  factory NotificationService() {
+    return _notificationService;
+  }
+  NotificationService._internal();
+
+  static const channelId = '123';
+
+  //instance of FlutterLocalNotificationsPlugin
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  Future<void> init() async {
+    final AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    final IOSInitializationSettings initializationSettingsIOS =
+        IOSInitializationSettings(
+      requestSoundPermission: false,
+      requestBadgePermission: false,
+      requestAlertPermission: false,
+    );
+
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+            android: initializationSettingsAndroid,
+            iOS: initializationSettingsIOS,
+            macOS: null);
+
+    tz.initializeTimeZones();
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onSelectNotification: selectNotification,
+    );
+  }
+
+  AndroidNotificationDetails _androidNotificationDetails =
+      AndroidNotificationDetails(
+    'channel ID',
+    'channel name',
+    channelDescription: 'channel description',
+    playSound: true,
+    priority: Priority.high,
+    importance: Importance.high,
+  );
+
+  Future<void> showNotifications() async {
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      "Notification Title",
+      "This is the Notification Body!",
+      NotificationDetails(android: _androidNotificationDetails),
+    );
+  }
+
+  Future<void> scheduleNotifications() async {
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+        0,
+        "Notification of Keyway",
+        "This is the Notification Body of Keyway app!",
+        tz.TZDateTime.now(tz.local).add(const Duration(seconds: 20)),
+        NotificationDetails(android: _androidNotificationDetails),
+        androidAllowWhileIdle: true,
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime);
+  }
+
+  Future<void> cancelNotifications(int id) async {
+    await flutterLocalNotificationsPlugin.cancel(id);
+  }
+
+  Future<void> cancelAllNotifications() async {
+    await flutterLocalNotificationsPlugin.cancelAll();
+  }
+}
+
+Future<String?> selectNotification(String? payload) async {
+  //handle your logic here
+}
