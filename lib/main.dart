982b13ae22dfd481754ef5c348672f3c0e9663cc
fiorcode <fiorcode@users.noreply.github.com>
2021-12-10 20:36:36 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
@@ -52,7 +54,17 @@ final AndroidInitializationSettings initializationSettingsAndroid =
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   NotificationService().init();
+  HttpOverrides.global = new MyHttpOverrides();
   runApp(MyApp());
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
 }
 
 class MyApp extends StatelessWidget {
